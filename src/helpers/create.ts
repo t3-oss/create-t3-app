a5500afc343966aff786687dd73be38d186a8c1d
Shoubhit Dash <shoubhit2005@gmail.com>
2022-05-24 12:54:22 +0530

    implement getPkgManager

--- src/helpers/create.ts
+++ src/helpers/create.ts
@@ -1,9 +1,12 @@
 import path from "path";
 import fs from "fs-extra";
+import getPkgManager from "./getPkgManager";
 
-export const createProject = (projectName: string) => {
+const createProject = (projectName: string) => {
   const srcDir = `${path.resolve(__dirname)}/../../template`;
   const projectDir = `./${projectName}`;
+
+  const pkgManager = getPkgManager();
 
   if (fs.existsSync(projectDir)) {
     throw new Error(`${projectName} already exists.`);
@@ -12,4 +15,17 @@ export const createProject = (projectName: string) => {
   fs.copy(srcDir, projectDir);
 
   console.log(`${projectName} created successfully.`);
+  console.log(`Next steps:`);
+  console.log(`- cd ${name}`);
+  console.log(`- ${pkgManager} install`);
+
+  if (pkgManager === "yarn") {
+    console.log(`- yarn dev`);
+  } else if (pkgManager === "pnpm") {
+    console.log(`- pnpm dev`);
+  } else {
+    console.log(`- npm run dev`);
+  }
 };
+
+export default createProject;
