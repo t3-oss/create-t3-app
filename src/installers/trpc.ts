b10d6b7ddd5441439c5bdc9d8a9a82c4f3d0b65f
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-06-20 01:35:53 +0200

    restructure trpc to match t3

--- src/installers/trpc.ts
+++ src/installers/trpc.ts
@@ -17,15 +17,33 @@ export const trpcInstaller = async (
 
   const trpcAssetDir = path.join(__dirname, "../../", "template/addons/trpc");
 
-  const appFile = path.join(trpcAssetDir, "_app.tsx");
+  const appSrc = path.join(trpcAssetDir, "_app.tsx");
   const appDest = path.join(projectDir, "src/pages/_app.tsx");
-  await fs.copy(appFile, appDest);
 
-  const apiHandlerFile = path.join(trpcAssetDir, "api-handler.ts");
+  const apiHandlerSrc = path.join(trpcAssetDir, "api-handler.ts");
   const apiHandlerDest = path.join(projectDir, "src/pages/api/trpc/[trpc].ts");
-  await fs.copy(apiHandlerFile, apiHandlerDest);
 
-  const utilsFile = path.join(trpcAssetDir, "utils.ts");
+  const utilsSrc = path.join(trpcAssetDir, "utils.ts");
   const utilsDest = path.join(projectDir, "src/utils/trpc.ts");
-  await fs.copy(utilsFile, utilsDest);
+
+  const contextSrc = path.join(trpcAssetDir, "base-context.ts");
+  const contextDest = path.join(projectDir, "src/server/router/context.ts");
+
+  const routerSrc = path.join(trpcAssetDir, "index-router.ts");
+  const routerDest = path.join(projectDir, "src/server/router/index.ts");
+
+  const exampleRouterSrc = path.join(trpcAssetDir, "example-router.ts");
+  const exampleRouterDest = path.join(
+    projectDir,
+    "src/server/router/example.ts"
+  );
+
+  await Promise.all([
+    fs.copy(appSrc, appDest),
+    fs.copy(apiHandlerSrc, apiHandlerDest),
+    fs.copy(utilsSrc, utilsDest),
+    fs.copy(contextSrc, contextDest),
+    fs.copy(routerSrc, routerDest),
+    fs.copy(exampleRouterSrc, exampleRouterDest),
+  ]);
 };
--- template/addons/trpc/api-handler.ts
+++ template/addons/trpc/api-handler.ts
@@ -1,29 +1,10 @@
-import * as trpc from "@trpc/server";
-import * as trpcNext from "@trpc/server/adapters/next";
-import superjson from "superjson";
-import { z } from "zod";
-
-export const appRouter = trpc
-  .router()
-  .transformer(superjson)
-  .query("hello", {
-    input: z
-      .object({
-        text: z.string().nullish(),
-      })
-      .nullish(),
-    resolve({ input }) {
-      return {
-        greeting: `Hello ${input?.text ?? "world"}`,
-      };
-    },
-  });
-
-// export type definition of API
-export type AppRouter = typeof appRouter;
+// src/pages/api/trpc/[trpc].ts
+import { createNextApiHandler } from "@trpc/server/adapters/next";
+import { appRouter } from "../../../server/router";
+import { createContext } from "../../../server/router/context";
 
 // export API handler
-export default trpcNext.createNextApiHandler({
+export default createNextApiHandler({
   router: appRouter,
-  createContext: () => null,
+  createContext: createContext,
 });
