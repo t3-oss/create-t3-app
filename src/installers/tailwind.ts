daa37363b06a70f66d1e1e6e6971e8b5803de065
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-06-19 21:22:48 +0200

    tw installer done

--- src/installers/tailwind.ts
+++ src/installers/tailwind.ts
@@ -1,7 +1,34 @@
-// Steps: https://tailwindcss.com/docs/guides/nextjs
+import { type PackageManager } from "../helpers/getPkgManager";
+import { exec } from "child_process";
+import { promisify } from "util";
+import fs from "fs-extra";
+import path from "path";
 
-// 1. Install dependencies
-// pnpx i -D tailwindcss postcss autoprefixer
+const execa = promisify(exec);
 
-// 2. Copy getting-started files
-// addons/tailwind/*
+export const tailwindInstaller = async (
+  projectDir: string,
+  pkgManager: PackageManager
+) => {
+  await execa(`${pkgManager} install -D tailwindcss postcss autoprefixer`, {
+    cwd: projectDir,
+  });
+
+  const twAssetDir = path.join(__dirname, "../../", "template/addons/tailwind");
+
+  const twConfig = path.join(twAssetDir, "tailwind.config.js");
+  const twDest = path.join(projectDir, "tailwind.config.js");
+  await fs.copy(twConfig, twDest);
+
+  const postcssConfig = path.join(twAssetDir, "postcss.config.js");
+  const postcssDest = path.join(projectDir, "postcss.config.js");
+  await fs.copy(postcssConfig, postcssDest);
+
+  const cssFile = path.join(twAssetDir, "globals.css");
+  const cssDest = path.join(projectDir, "src/styles/globals.css");
+  await fs.copy(cssFile, cssDest);
+
+  const indexFile = path.join(twAssetDir, "..", "page-examples/tailwind.tsx");
+  const indexDest = path.join(projectDir, "src/pages/index.tsx");
+  await fs.copy(indexFile, indexDest);
+};
