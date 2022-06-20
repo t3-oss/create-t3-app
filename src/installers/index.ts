ad71afafec049dbad099f5fafd6dc55de4ed89c1
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-06-20 16:00:22 +0200

    type refactoring

--- src/installers/index.ts
+++ src/installers/index.ts
@@ -3,19 +3,7 @@ import { trpcInstaller } from "./trpc";
 import { prismaInstaller } from "./prisma";
 // import { nextAuthInstaller } from "./next-auth";
 
-import type { Packages } from "../index";
-import { PackageManager } from "../helpers/getPkgManager";
-
-export type Installer = (
-  projectDir: string,
-  pkgManager: PackageManager,
-  packages?: Packages
-) => Promise<void>;
-export type Installers = {
-  [pkgName in keyof Packages]: Installer;
-};
-
-export const installers: Installers = {
+export const installers = {
   tailwind: tailwindInstaller,
   trpc: trpcInstaller,
   prisma: prismaInstaller,
