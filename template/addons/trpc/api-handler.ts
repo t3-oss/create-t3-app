155d882a658f2e2978b6318a8fdc810b249a5f94
Vitor Lima <vitorwlima13@gmail.com>
2022-06-22 00:46:41 -0300

    feat: add inferQueryResponse helper for tRPC

--- template/addons/trpc/api-handler.ts
+++ template/addons/trpc/api-handler.ts
@@ -1,6 +1,7 @@
 // src/pages/api/trpc/[trpc].ts
+import { inferProcedureOutput } from "@trpc/server";
 import { createNextApiHandler } from "@trpc/server/adapters/next";
-import { appRouter } from "../../../server/router";
+import { AppRouter, appRouter } from "../../../server/router";
 import { createContext } from "../../../server/router/context";
 
 // export API handler
@@ -8,3 +9,7 @@ export default createNextApiHandler({
   router: appRouter,
   createContext: createContext,
 });
+
+export type inferQueryResponse<
+  TRouteKey extends keyof AppRouter["_def"]["queries"]
+> = inferProcedureOutput<AppRouter["_def"]["queries"][TRouteKey]>;
