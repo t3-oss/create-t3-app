6ea196267453916bba45f874f904404a9879e02e
Michael White <michael@mwhite.io>
2022-06-26 15:05:08 -0400

    Move cli tools into ./cli

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,16 @@
 #!/usr/bin/env node
 import type { PackageJson } from "type-fest";
 import path from "path";
-import chalk from "chalk";
-import { Command } from "commander";
 import fs from "fs-extra";
-import prompts from "prompts";
-import { CREATE_T3_APP, TITLE_TEXT, DEFAULT_APP_NAME } from "./consts";
+import { runCli } from "./cli";
+import { TITLE_TEXT } from "./consts";
 import { createProject } from "./helpers/createProject";
 import { initializeGit } from "./helpers/initGit";
 import { logNextSteps } from "./helpers/logNextSteps";
 import { installers, type Installer } from "./installers";
-import { getVersion } from "./utils/getT3Version";
 import { logger } from "./utils/logger";
 
-type AvailablePackages = "tailwind" | "trpc" | "prisma" | "nextAuth";
+export type AvailablePackages = "tailwind" | "trpc" | "prisma" | "nextAuth";
 export type Packages = {
   [pkg in AvailablePackages]: {
     inUse: boolean;
@@ -21,150 +18,34 @@ export type Packages = {
   };
 };
 
-const program = new Command().name(CREATE_T3_APP);
-
 const main = async () => {
   logger.error(TITLE_TEXT, "\n");
 
-  program
-    .description("A CLI for creating web applications with the t3 stack")
-    .argument(
-      "[dir]",
-      "The name of the application, as well as the name of the directory to create",
-    )
-    .option(
-      "--noGit",
-      "Explicitly tell the CLI to not initialize a new git repo in the project",
-    )
-    .option(
-      "--noInstall",
-      "Explicitly tell the CLI to not run the package manager's install command",
-    )
-    .option(
-      "-y, --default",
-      "Bypass the CLI and use all default options to bootstrap a new t3-app",
-    )
-    .version(getVersion(), "-v, --version", "Display the version number")
-    .addHelpText(
-      "afterAll",
-      `\n The t3 stack was inspired by ${chalk
-        .hex("#E8DCFF")
-        .bold(
-          "@t3dotgg",
-        )} and has been used to build awesome fullstack applications like ${chalk
-        .hex("#E24A8D")
-        .underline("https://ping.gg")} \n`,
-    )
-    .parse(process.argv);
-
-  const programOptions = program.opts<{
-    noGit?: boolean;
-    noInstall?: boolean;
-    default?: boolean;
-  }>();
-
-  const cliProvidedName: string | undefined = program.args[0];
-
-  //DEV
-  console.log({
-    cliProvidedName,
-    programOptions,
-  });
-
-  // FIXME: Look into if the type can be inferred
-  const { name, useTailwind, useTrpc, usePrisma, useNextAuth } = (await prompts(
-    [
-      {
-        name: "name",
-        type: !!cliProvidedName ? null : "text",
-        message: "What will your project be called?",
-        initial: DEFAULT_APP_NAME,
-        format: (input: string) => {
-          return input.trim();
-        },
-      },
-      {
-        name: "language",
-        type: "select",
-        message: "Will you be using JavaScript or TypeScript?",
-        instructions: false,
-        choices: [
-          {
-            title: "TypeScript",
-            value: "typescript",
-          },
-          {
-            title: "JavaScript",
-            value: "javascript",
-          },
-        ],
-        format: (language: string) => {
-          if (language === "javascript") {
-            logger.error("Wrong answer, using TypeScript instead...");
-          } else {
-            logger.success("Good choice! Using TypeScript!");
-          }
-          return;
-        },
-      },
-      {
-        name: "useTailwind",
-        type: "toggle",
-        message: "Would you like to use Tailwind?",
-        initial: true,
-        active: "Yes",
-        inactive: "No",
-      },
-      {
-        name: "useTrpc",
-        type: "toggle",
-        message: "Would you like to use tRPC?",
-        initial: true,
-        active: "Yes",
-        inactive: "No",
-      },
-      {
-        name: "usePrisma",
-        type: "toggle",
-        message: "Would you like to use Prisma?",
-        initial: true,
-        active: "Yes",
-        inactive: "No",
-      },
-      {
-        name: "useNextAuth",
-        type: "toggle",
-        message: "Would you like to use Next Auth?",
-        initial: true,
-        active: "Yes",
-        inactive: "No",
-      },
-    ],
-  )) as {
-    name: string;
-    useTailwind: boolean;
-    useTrpc: boolean;
-    usePrisma: boolean;
-    useNextAuth: boolean;
-  };
+  const {
+    appName,
+    usePackages: { nextAuth, prisma, tailwind, trpc },
+    flags,
+  } = await runCli();
 
   const packages: Packages = {
-    tailwind: { inUse: useTailwind, installer: installers.tailwind },
-    trpc: { inUse: useTrpc, installer: installers.trpc },
-    prisma: { inUse: usePrisma, installer: installers.prisma },
-    nextAuth: { inUse: useNextAuth, installer: installers.nextAuth },
+    tailwind: { inUse: tailwind, installer: installers.tailwind },
+    trpc: { inUse: trpc, installer: installers.trpc },
+    prisma: { inUse: prisma, installer: installers.prisma },
+    nextAuth: { inUse: nextAuth, installer: installers.nextAuth },
   };
 
-  const projectDir = await createProject(name, packages);
+  const projectDir = await createProject(appName, packages);
 
-  await initializeGit(projectDir);
+  if (!flags.noGit) {
+    await initializeGit(projectDir);
+  }
 
-  logNextSteps(name, packages);
+  logNextSteps(appName, packages);
 
   const pkgJson = (await fs.readJSON(
     path.join(projectDir, "package.json"),
   )) as PackageJson;
-  pkgJson.name = name;
+  pkgJson.name = appName;
   await fs.writeJSON(path.join(projectDir, "package.json"), pkgJson, {
     spaces: 2,
   });
