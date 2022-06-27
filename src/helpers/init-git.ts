4c00eb3f042513593f542de4dc14f7360cce4d3e
Dhravya <dhravyashah@gmail.com>
2022-06-27 17:21:15 +0530

    Added loader when installing packages

--- src/helpers/init-git.ts
+++ src/helpers/init-git.ts
@@ -1,19 +1,22 @@
 import path from "path";
 import chalk from "chalk";
 import fs from "fs-extra";
+import ora from "ora";
 import { execa } from "./execa";
 import { logger } from "./logger";
 
 // This initializes the Git-repository for the project
 export const initializeGit = async (projectDir: string) => {
-  logger.info("Initializing git...");
-
+  const spinner = ora("Initializing Git...\n");
+  spinner.color = "blue";
+  spinner.start();
   try {
     await execa("git init", { cwd: projectDir });
     logger.success(`${chalk.bold.green("Finished")} initializing git\n`);
   } catch (error) {
     logger.error(`${chalk.bold.red("Failed: ")} could not initialize git\n`);
   }
+  spinner.stop();
 
   await fs.rename(
     path.join(projectDir, "_gitignore"),
