30dda506d0d9d5f94af21058eeeb39d889a88118
Djordje Stevanovic <EvonuX@users.noreply.github.com>
2022-06-27 16:05:56 +0200

    feat: use path alias when importing

--- template/addons/trpc/example-router.ts
+++ template/addons/trpc/example-router.ts
@@ -1,4 +1,4 @@
-import { createRouter } from "./context";
+import { createRouter } from "~/context";
 import { z } from "zod";
 
 export const exampleRouter = createRouter().query("hello", {
--- template/addons/trpc/index-router.ts
+++ template/addons/trpc/index-router.ts
@@ -1,8 +1,8 @@
 // src/server/router/index.ts
-import { createRouter } from "./context";
+import { createRouter } from "~/context";
 import superjson from "superjson";
 
-import { exampleRouter } from "./example";
+import { exampleRouter } from "~/example";
 
 export const appRouter = createRouter()
   .transformer(superjson)
