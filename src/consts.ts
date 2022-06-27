0937935f4400ffca9b9b1b0de7307b0a5e031bfe
Michael White <michael@mwhite.io>
2022-06-27 17:20:15 -0400

    Outputting to ESM now
    Removed __dirname as required by esm

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ import path from "path";
 
 // With the move to TSUP as a build tool, this keeps path routes in other files (installers, loaders, etc) in check more easily.
 // Path is in relation to a single index.js file inside ./dist
-export const PKG_ROOT = path.join(__dirname, "../");
+export const PKG_ROOT = path.dirname("../");
 
 export const TITLE_TEXT = `Welcome to the create-t3-app !`;
 export const DEFAULT_APP_NAME = "my-t3-app";
