2236c9b72795d692fdd00958fb7580a152acf847
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-06-28 12:01:32 +0200

    accurately resolve PKG_ROOT

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,13 @@
 import path from "path";
+import { fileURLToPath } from "url";
 
 // With the move to TSUP as a build tool, this keeps path routes in other files (installers, loaders, etc) in check more easily.
 // Path is in relation to a single index.js file inside ./dist
-export const PKG_ROOT = path.dirname("../");
+const __filename = fileURLToPath(import.meta.url);
+const distPath = path.dirname(__filename);
+export const PKG_ROOT = path.join(distPath, "../");
+
+//export const PKG_ROOT = path.dirname(require.main.filename);
 
 export const TITLE_TEXT = `Welcome to the create-t3-app !`;
 export const DEFAULT_APP_NAME = "my-t3-app";
