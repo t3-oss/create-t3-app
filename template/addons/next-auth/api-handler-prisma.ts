5a2a2adc718477d8a9b93f9b2ffbe60d16b7be60
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-06-28 20:55:16 +0200

    sample credential provider for testing

--- template/addons/next-auth/api-handler-prisma.ts
+++ template/addons/next-auth/api-handler-prisma.ts
@@ -1,5 +1,6 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
@@ -14,6 +15,20 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
     // ...add more providers here
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        name: {
+          label: "Name",
+          type: "text",
+          placeholder: "Enter your name",
+        },
+      },
+      async authorize(credentials, _req) {
+        const user = { id: 1, name: credentials?.name ?? "J Smith" };
+        return user;
+      },
+    }),
   ],
 };
 
