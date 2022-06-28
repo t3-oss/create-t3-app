f34cb78884b9a24afd67caf3cf0343cd8bf704c3
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-06-28 17:20:35 +0200

    refactor authOptions

--- template/addons/next-auth/api-handler-prisma.ts
+++ template/addons/next-auth/api-handler-prisma.ts
@@ -1,11 +1,11 @@
-import NextAuth from "next-auth";
+import NextAuth, { type NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../server/db/client";
 
-export default NextAuth({
+export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
@@ -15,4 +15,6 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-});
+};
+
+export default NextAuth(authOptions);
