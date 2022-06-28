122cf2fa72123b295b41a250ae606321215dd059
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-06-28 21:03:47 +0200

    updated installer for new changes

--- src/installers/trpc.ts
+++ src/installers/trpc.ts
@@ -23,6 +23,8 @@ export const trpcInstaller: Installer = async (
     ],
     devMode: false,
   });
+  const usingAuth = packages.nextAuth.inUse;
+  const usingPrisma = packages.prisma.inUse;
 
   const trpcAssetDir = path.join(PKG_ROOT, "template/addons/trpc");
 
@@ -32,19 +34,34 @@ export const trpcInstaller: Installer = async (
   const utilsSrc = path.join(trpcAssetDir, "utils.ts");
   const utilsDest = path.join(projectDir, "src/utils/trpc.ts");
 
-  const contextSrc = path.join(
-    trpcAssetDir,
-    packages.prisma.inUse ? "prisma-context.ts" : "base-context.ts",
-  );
+  const contextFile =
+    usingAuth && usingPrisma
+      ? "auth-prisma-context.ts"
+      : usingAuth && !usingPrisma
+      ? "auth-context.ts"
+      : !usingAuth && usingPrisma
+      ? "prisma-context.ts"
+      : "base-context.ts";
+  const contextSrc = path.join(trpcAssetDir, contextFile);
   const contextDest = path.join(projectDir, "src/server/router/context.ts");
 
-  const routerSrc = path.join(trpcAssetDir, "index-router.ts");
-  const routerDest = path.join(projectDir, "src/server/router/index.ts");
+  if (usingAuth) {
+    const authRouterSrc = path.join(trpcAssetDir, "auth-router.ts");
+    const authRouterDest = path.join(projectDir, "src/server/router/auth.ts");
+    console.log("copying over");
+    await fs.copy(authRouterSrc, authRouterDest);
+  }
 
-  const exampleRouterSrc = path.join(
-    trpcAssetDir,
-    packages.prisma.inUse ? "example-prisma-router.ts" : "example-router.ts",
-  );
+  const indexRouterFile = usingAuth
+    ? "auth-index-router.ts"
+    : "index-router.ts";
+  const indexRouterSrc = path.join(trpcAssetDir, indexRouterFile);
+  const indexRouterDest = path.join(projectDir, "src/server/router/index.ts");
+
+  const exampleRouterFile = usingPrisma
+    ? "example-prisma-router.ts"
+    : "example-router.ts";
+  const exampleRouterSrc = path.join(trpcAssetDir, exampleRouterFile);
   const exampleRouterDest = path.join(
     projectDir,
     "src/server/router/example.ts",
@@ -54,7 +71,7 @@ export const trpcInstaller: Installer = async (
     fs.copy(apiHandlerSrc, apiHandlerDest),
     fs.copy(utilsSrc, utilsDest),
     fs.copy(contextSrc, contextDest),
-    fs.copy(routerSrc, routerDest),
+    fs.copy(indexRouterSrc, indexRouterDest),
     fs.copy(exampleRouterSrc, exampleRouterDest),
   ]);
 };
