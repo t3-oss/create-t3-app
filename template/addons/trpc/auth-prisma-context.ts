64ed259032732c5117dc5664708aaffc28c18b56
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-06-28 20:59:58 +0200

    modify context

--- template/addons/trpc/auth-prisma-context.ts
+++ template/addons/trpc/auth-prisma-context.ts
@@ -1,7 +1,7 @@
 // src/server/router/context.ts
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
-import { getServerSideSession } from "next-auth";
+import { unstable_getServerSession as getServerSession } from "next-auth";
 
 import { authOptions as nextAuthOptions } from "../../pages/api/auth/[...nextauth]";
 import { prisma } from "../db/client";
@@ -12,7 +12,8 @@ export const createContext = async (
   const req = opts?.req;
   const res = opts?.res;
 
-  const session = opts && (await getServerSession(opts, nextAuthOptions));
+  const session =
+    req && res && (await getServerSession(req, res, nextAuthOptions));
 
   return {
     req,
--- template/addons/trpc/prisma-context.ts
+++ template/addons/trpc/prisma-context.ts
@@ -3,10 +3,10 @@ import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 import { prisma } from "../db/client";
 
-export const createContext = ({
-  req,
-  res,
-}: trpcNext.CreateNextContextOptions) => {
+export const createContext = (opts?: trpcNext.CreateNextContextOptions) => {
+  const req = opts?.req;
+  const res = opts?.res;
+
   return {
     req,
     res,
