d48e09f7e401743cc4751ed43ff9fdf6aa94ad3a
Colin Leahy <colinleahy@Colins-MBP.hsd1.co.comcast.net>
2022-07-04 01:25:55 -0600

    heading formatting and remove cursor for non link elem

--- template/page-studs/index/with-tw.tsx
+++ template/page-studs/index/with-tw.tsx
@@ -15,12 +15,11 @@ const Home: NextPage = () => {
           Create <span className="text-blue-500">T3</span> App
         </h1>
 
-        <h3 className="items-center m-5 text-3xl">This Stack uses:-</h3>
+        <h3 className="items-center m-5 text-3xl">This stack uses:</h3>
         <main className="grid items-start grid-cols-1 gap-10 p-5 md:p-0 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-          
           <section className="h-full max-h-72 transform group border-2 border-neutral-800 rounded-2xl duration-300 hover:scale-105 hover:border-blue-600 hover:-translate-y-1 hover:shadow-2xl">
             <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-              <div className="p-5 py-10 flex flex-col justify-center h-full cursor-pointer text-center">
+              <div className="p-5 py-10 flex flex-col justify-center h-full text-center">
                 <h2 className="mb-5 text-3xl">NextJS</h2>
                 <p className="mb-5">The React framework for production</p>
                 <button className="p-2 px-6 w-fit self-center text-white font-bold bg-blue-500 rounded-full group-hover:bg-blue-600 duration-300">
@@ -31,8 +30,12 @@ const Home: NextPage = () => {
           </section>
 
           <section className="h-full max-h-72 transform group border-2 border-neutral-800 rounded-2xl duration-300 hover:scale-105 hover:border-blue-600 hover:-translate-y-1 hover:shadow-2xl">
-            <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer">
-              <div className="p-5 py-10 flex flex-col justify-center h-full cursor-pointer text-center">
+            <a
+              href="https://www.typescriptlang.org/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="p-5 py-10 flex flex-col justify-center h-full text-center">
                 <h2 className="mb-5 text-3xl">TypeScript</h2>
                 <p className="mb-5">
                   Strongly typed programming language that builds on JavaScript,
@@ -47,7 +50,7 @@ const Home: NextPage = () => {
 
           <section className="h-full max-h-72 transform group border-2 border-neutral-800 rounded-2xl duration-300 hover:scale-105 hover:border-blue-600 hover:-translate-y-1 hover:shadow-2xl">
             <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer">
-              <div className="p-5 py-10 flex flex-col justify-center h-full cursor-pointer text-center">
+              <div className="p-5 py-10 flex flex-col justify-center h-full text-center">
                 <h2 className="mb-5 text-3xl">TailwindCSS</h2>
                 <p className="mb-5">
                   Rapidly build modern websites without ever leaving your HTML
@@ -58,7 +61,6 @@ const Home: NextPage = () => {
               </div>
             </a>
           </section>
-
         </main>
       </div>
     </>
