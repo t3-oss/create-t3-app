485acd12e5f5007bcecec1eb7e2356a31602a220
ashish <asheeshh@duck.com>
2022-07-05 00:50:23 +0530

    feat: make the ui symmetric

--- template/page-studs/index/with-tw.tsx
+++ template/page-studs/index/with-tw.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
         <h2 className="text-[3rem] lg:text-[5rem] md:text-[5rem] font-extrabold text-gray-700">
           Create <span className="text-purple-300">T3</span> App
         </h2>
-        <p className="text-lg text-gray-700">This stack uses -</p>
+        <p className="text-2xl text-gray-700">This stack uses</p>
         <div className="grid grid-cols-1 grid-rows-3 lg:grid-rows-1 md:grid-rows-1 lg:grid-cols-3 md:grid-cols-3 gap-3 mt-3 pt-3 w-full lg:w-2/3 md:w-full">
           <div className="hover:scale-105 cursor-pointer duration-500 flex flex-col justify-center items-center text-center rounded shadow-xl border-2 border-gray-500 h-full w-full p-6">
             <h2 className="text-lg text-gray-700">NextJS</h2>
