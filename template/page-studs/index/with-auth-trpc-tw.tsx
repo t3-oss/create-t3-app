19df71ec53fd64e60b27ecd055cd9f0c6acc4a63
ashish <asheeshh@duck.com>
2022-07-05 00:50:23 +0530

    feat: make the ui symmetric

--- template/page-studs/index/with-auth-trpc-tw.tsx
+++ template/page-studs/index/with-auth-trpc-tw.tsx
@@ -34,11 +34,11 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="w-screen h-screen flex flex-col justify-center items-center p-4 overflow-y-scroll">
+      <div className="w-screen min-h-screen flex flex-col justify-center items-center p-4 overflow-y-scroll">
         <h2 className="text-[3rem] lg:text-[5rem] md:text-[5rem] font-extrabold text-gray-700">
           Create <span className="text-purple-300">T3</span> App
         </h2>
-        <p className="text-lg text-gray-700">This stack uses -</p>
+        <p className="text-2xl text-gray-700">This stack uses</p>
         <div className="grid grid-cols-1 grid-rows-3 lg:grid-rows-2 md:grid-rows-2 justify-center items-center lg:grid-cols-3 md:grid-cols-3 gap-3 mt-3 pt-3 w-full lg:w-2/3 md:w-full">
           <div className="hover:scale-105 cursor-pointer duration-500 flex flex-col justify-center items-center text-center rounded shadow-xl border-2 border-gray-500 h-full w-full p-6">
             <h2 className="text-lg text-gray-700">NextJS</h2>
@@ -83,7 +83,7 @@ const Home: NextPage = () => {
               Documentation
             </a>
           </div>
-          <div className="lg:col-span-2 hover:scale-105 cursor-pointer duration-500 flex flex-col justify-center items-center text-center rounded shadow-xl border-2 border-gray-500 h-full w-full p-6">
+          <div className="hover:scale-105 cursor-pointer duration-500 flex flex-col justify-center items-center text-center rounded shadow-xl border-2 border-gray-500 h-full w-full p-6">
             <h2 className="text-lg text-gray-700">tRPC</h2>
             <p className="text-sm text-gray-600">
               End-to-end typesafe APIs made easy
@@ -103,6 +103,20 @@ const Home: NextPage = () => {
             <a
               className="text-sm text-violet-500 underline decoration-dotted underline-offset-2 cursor-pointer mt-3"
               href="https://trpc.io/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Documentation
+            </a>
+          </div>
+          <div className="hover:scale-105 cursor-pointer duration-500 flex flex-col justify-center items-center text-center rounded shadow-xl border-2 border-gray-500 h-full w-full p-6">
+            <h2 className="text-lg text-gray-700">Prisma</h2>
+            <p className="text-sm text-gray-600">
+              Build data-driven JavaScript & TypeScript apps in less time
+            </p>
+            <a
+              className="text-sm text-violet-500 underline decoration-dotted underline-offset-2 cursor-pointer mt-3"
+              href="https://www.prisma.io/docs/"
               target="_blank"
               rel="noreferrer"
             >
