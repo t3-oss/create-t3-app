105600a110d5465a6e0d2074e2d64b53ffe49615
mnik01 <maksimgrinberg01@gmail.com>
2022-07-05 16:09:53 +0600

    fix(templates): h1 restored

--- template/page-studs/index/with-trpc-tw.tsx
+++ template/page-studs/index/with-trpc-tw.tsx
@@ -14,9 +14,9 @@ const Home: NextPage = () => {
       </Head>
 
       <div className="w-screen min-h-screen flex flex-col justify-center items-center p-4 overflow-y-scroll">
-        <h2 className="text-[3rem] lg:text-[5rem] md:text-[5rem] font-extrabold text-gray-700">
+        <h1 className="text-[3rem] lg:text-[5rem] md:text-[5rem] font-extrabold text-gray-700">
           Create <span className="text-purple-300">T3</span> App
-        </h2>
+        </h1>
         <p className="text-2xl text-gray-700">This stack uses</p>
         <div className="grid grid-cols-1 grid-rows-3 lg:grid-rows-2 md:grid-rows-2 justify-center items-center lg:grid-cols-2 md:grid-cols-2 gap-3 mt-3 pt-3 w-full lg:w-2/3 md:w-full">
           <div className="hover:scale-105 cursor-pointer duration-500 flex flex-col justify-center items-center text-center rounded shadow-xl border-2 border-gray-500 h-full w-full p-6">
