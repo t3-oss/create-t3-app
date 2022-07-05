a8d88477dfb9b0a242913123686ef04ea674a4df
Federico Orlandau <federico.orlandau@gmail.com>
2022-07-06 00:54:36 +0200

    fix: add comment for middleware example

--- template/addons/next-auth/middleware.ts
+++ template/addons/next-auth/middleware.ts
@@ -1,10 +1,9 @@
+// Example of a restricted route that only authenticated users can access https://next-auth.js.org/configuration/nextjs#middleware
+
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-/**
- * This middleware will redirect to the home page if the user is not signed in and try to enter to "/profile".
- */
 export async function middleware(request: NextRequest) {
   const token = await getToken({
     req: request,
