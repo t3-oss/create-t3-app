908630e5cc14789b88a0ddf6dd097fb14eb9068a
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-07-07 13:52:30 +0200

    fix: type entrire app and not just the props

--- template/base/src/pages/_app.tsx
+++ template/base/src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/dist/shared/lib/utils";
+import type { AppType } from "next/dist/shared/lib/utils";
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
-}
+};
 
 export default MyApp;
--- template/page-studs/_app/with-auth.tsx
+++ template/page-studs/_app/with-auth.tsx
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/dist/shared/lib/utils";
+import type { AppType } from "next/dist/shared/lib/utils";
 import { SessionProvider } from "next-auth/react";
 
-const MyApp = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
+const MyApp: AppType = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />;
     </SessionProvider>
   );
-}
+};
 
 export default MyApp;
