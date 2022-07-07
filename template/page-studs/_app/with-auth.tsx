e0a13665f5e5549f50a073e4a4584dadf840a229
Fizzify <fizzyplayzoriginal5@gmail.com>
2022-07-07 19:46:58 +0800

    Make _app.tsx and with-auth.tsx use arrow functions.

--- template/page-studs/_app/with-auth.tsx
+++ template/page-studs/_app/with-auth.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/dist/shared/lib/utils";
 import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+const MyApp = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />;
