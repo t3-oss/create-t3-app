972d2115520888be885cfd8a011fd364baec9a96
thegeeko <abdelhadims@icloud.com>
2022-07-09 17:04:12 +0200

    fix: replaced process with typeof window since it's deprecated

--- template/page-studs/_app/with-trpc.tsx
+++ template/page-studs/_app/with-trpc.tsx
@@ -13,7 +13,7 @@ const getBaseUrl = () => {
   if (typeof window !== "undefined") {
     return "";
   }
-  if (process.browser) return ""; // Browser should use current path
+  if (typeof window !== "undefined") return ""; // Browser should use current path
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
 
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
