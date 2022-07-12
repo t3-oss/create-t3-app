848db1f7103c19f859f011d6f2f921b33f7725f7
Markas Klapovščiuk <48568793+markkkkas@users.noreply.github.com>
2022-07-12 15:23:53 +0300

    Remove process.browser check because its deprecated (#174)
    Reference: https://github.com/vercel/next.js/issues/5354#issuecomment-520305040
    
    Co-authored-by: Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>

--- template/page-studs/_app/with-auth-trpc.tsx
+++ template/page-studs/_app/with-auth-trpc.tsx
@@ -21,7 +21,6 @@ const getBaseUrl = () => {
   if (typeof window !== "undefined") {
     return "";
   }
-  if (process.browser) return ""; // Browser should use current path
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
 
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
