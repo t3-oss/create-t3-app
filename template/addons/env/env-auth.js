fb363e9a52b59114113fc4c0682f06e3625a53e4
Ekaansh Arora <blue.name68@gmail.com>
2022-07-13 21:10:17 +0530

    chore: use discord for default auth (#187)

--- template/addons/env/env-auth.js
+++ template/addons/env/env-auth.js
@@ -3,8 +3,8 @@ const { z } = require("zod");
 const envSchema = z.object({
   NEXTAUTH_SECRET: z.string(),
   NEXTAUTH_URL: z.string().url(),
-  GITHUB_ID: z.string(),
-  GITHUB_SECRET: z.string(),
+  DISCORD_CLIENT_ID: z.string(),
+  DISCORD_CLIENT_SECRET: z.string(),
 });
 
 module.exports.envSchema = envSchema;
--- template/addons/next-auth/api-handler.ts
+++ template/addons/next-auth/api-handler.ts
@@ -1,14 +1,14 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import DiscordProvider from "next-auth/providers/discord";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { env } from "../../../server/env";
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: env.GITHUB_ID,
-      clientSecret: env.GITHUB_SECRET,
+    DiscordProvider({
+      clientId: env.DISCORD_CLIENT_ID,
+      clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
     // ...add more providers here
     CredentialsProvider({
