4c162a7e5b2e9bce03d3e1a852de0e210982c110
Vikrant Bhat <50735025+bhatvikrant@users.noreply.github.com>
2022-07-14 11:02:12 +0530

    chore: include user.id on session (#180)
    * chore: include user.id on session
    
    * chore: include next-auth.d.ts only for next-auth scaffoldings
    
    * Update template/addons/next-auth/api-handler-prisma.ts
    
    Co-authored-by: Balázs Orbán <info@balazsorban.com>
    
    * Update template/addons/next-auth/api-handler.ts
    
    Co-authored-by: Balázs Orbán <info@balazsorban.com>
    
    * Update template/addons/next-auth/next-auth.d.ts
    
    Co-authored-by: Balázs Orbán <info@balazsorban.com>
    
    * fix: include `next-auth.d.ts` in `tsconfig.json`
    
    * fix: run prettier
    
    * chore: make `user` and `id` optional for better type-safety
    
    * fix: add `id` to `session.user` only when it exists
    
    Co-authored-by: Balázs Orbán <info@balazsorban.com>

--- template/addons/next-auth/api-handler-prisma.ts
+++ template/addons/next-auth/api-handler-prisma.ts
@@ -8,6 +8,15 @@ import { prisma } from "../../../server/db/client";
 import { env } from "../../../server/env";
 
 export const authOptions: NextAuthOptions = {
+  // Include user.id on session
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
