decb70da35b9a43e7028972c8c60798d542de785
Vikrant Bhat <vik.bhat223@gmail.com>
2022-07-14 16:13:32 +0530

    chore: add protected trpc router example

---
+++ template/addons/next-auth/protected-router.ts
@@ -0,0 +1,17 @@
+import * as trpc from "@trpc/server";
+import { createRouter } from "./context";
+
+export function createProtectedRouter() {
+  return createRouter().middleware(({ ctx, next }) => {
+    if (!ctx.session || !ctx.session.user) {
+      throw new trpc.TRPCError({ code: "UNAUTHORIZED" });
+    }
+    return next({
+      ctx: {
+        ...ctx,
+        // infers that `session` is non-nullable to downstream resolvers
+        session: { ...ctx.session, user: ctx.session.user },
+      },
+    });
+  });
+}
