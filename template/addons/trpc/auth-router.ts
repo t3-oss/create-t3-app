435dbc1e6c68547ca43c54623586b1dd7672d4a6
Vikrant Bhat <vik.bhat223@gmail.com>
2022-07-14 17:57:55 +0530

    chore: remove auth middleware from `auth-router.ts` since its now handled by `protected-router.ts`

--- template/addons/trpc/auth-router.ts
+++ template/addons/trpc/auth-router.ts
@@ -7,14 +7,6 @@ export const authRouter = createRouter()
       return ctx.session;
     },
   })
-  .middleware(async ({ ctx, next }) => {
-    // Any queries or mutations after this middleware will
-    // raise an error unless there is a current session
-    if (!ctx.session) {
-      throw new TRPCError({ code: "UNAUTHORIZED" });
-    }
-    return next();
-  })
   .query("getSecretMessage", {
     async resolve({ ctx }) {
       return "You are logged in and can see this secret message!";
