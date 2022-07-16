db4ad6aa1f5acc2e120201af4248002113abaf10
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-07-16 19:08:19 +0200

    feat: update frontend

--- template/page-studs/index/with-tw.tsx
+++ template/page-studs/index/with-tw.tsx
@@ -2,32 +2,6 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import React from "react";
 
-// Technology component
-interface TechnologyProps {
-  name: string;
-  description: string;
-  documentation: string;
-}
-
-const Technology: React.FC<TechnologyProps> = (props) => {
-  return (
-    <>
-      <div className="hover:scale-105 cursor-pointer duration-500 flex flex-col justify-center items-center text-center rounded shadow-xl border-2 border-gray-500 h-full w-full p-6">
-        <h2 className="text-lg text-gray-700">{props.name}</h2>
-        <p className="text-sm text-gray-600">{props.description}</p>
-        <a
-          className="text-sm text-violet-500 underline decoration-dotted underline-offset-2 cursor-pointer mt-3"
-          href={props.documentation}
-          target="_blank"
-          rel="noreferrer"
-        >
-          Documentation
-        </a>
-      </div>
-    </>
-  );
-};
-
 const Home: NextPage = () => {
   return (
     <>
@@ -37,12 +11,12 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="w-screen min-h-screen flex flex-col justify-center items-center p-4 overflow-y-scroll">
+      <div className="flex flex-col items-center justify-center w-screen min-h-screen p-4 overflow-y-scroll">
         <h2 className="text-[3rem] lg:text-[5rem] md:text-[5rem] font-extrabold text-gray-700">
           Create <span className="text-purple-300">T3</span> App
         </h2>
         <p className="text-2xl text-gray-700">This stack uses</p>
-        <div className="grid grid-cols-1 grid-rows-3 lg:grid-rows-2 md:grid-rows-2 justify-center items-center lg:grid-cols-2 md:grid-cols-2 gap-3 mt-3 pt-3 w-full lg:w-2/3 md:w-full">
+        <div className="grid items-center justify-center w-full grid-cols-1 grid-rows-3 gap-3 pt-3 mt-3 lg:grid-rows-2 md:grid-rows-2 lg:grid-cols-2 md:grid-cols-2 lg:w-2/3 md:w-full">
           <Technology
             name={"NextJS"}
             description={"The React framework for production"}
@@ -74,3 +48,29 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+// Technology component
+interface TechnologyProps {
+  name: string;
+  description: string;
+  documentation: string;
+}
+
+const Technology: React.FC<TechnologyProps> = (props) => {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center w-full h-full p-6 text-center duration-500 border-2 border-gray-500 rounded shadow-xl cursor-pointer hover:scale-105">
+        <h2 className="text-lg text-gray-700">{props.name}</h2>
+        <p className="text-sm text-gray-600">{props.description}</p>
+        <a
+          className="mt-3 text-sm underline cursor-pointer text-violet-500 decoration-dotted underline-offset-2"
+          href={props.documentation}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Documentation
+        </a>
+      </div>
+    </>
+  );
+};
