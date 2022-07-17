ccab745040c997900cd0ce20aae9bc451d62981a
Daniel Roe <daniel@roe.dev>
2022-07-17 17:16:06 +0100

    feat: shamelessly steal type safety from @KATT
    https://github.com/trpc/examples-next-prisma-starter/blob/main/src/utils/publicRuntimeConfig.ts

--- template/base/next.config.mjs
+++ template/base/next.config.mjs
@@ -1,8 +1,17 @@
 import { env } from "./src/server/env.mjs";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+/**
+ * Don't be scared of the generics here.
+ * All they do is to give us autocompletion when using this.
+ *
+ * @template {import('next').NextConfig} T
+ * @param {T} config - A generic parameter that flows through to the return type
+ * @constraint {{import('next').NextConfig}}
+ */
+function defineNextConfig(config) {
+  return config;
+}
 
-export default nextConfig;
+export default defineNextConfig({
+  reactStrictMode: true,
+});
