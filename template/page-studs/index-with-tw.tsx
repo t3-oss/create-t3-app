b9d6d7e63ce8a9754f1aff8d000b11006c437045
Carlos Gomez <76704548+CarlosGomez-dev@users.noreply.github.com>
2022-07-17 19:01:45 -0400

    feat: create generic index files

--- template/page-studs/index-with-tw.tsx
+++ template/page-studs/index-with-tw.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { trpc } from "../utils/trpc";
 
 type TechnologyCardProps = {
   name: string;
@@ -9,8 +8,6 @@ type TechnologyCardProps = {
 };
 
 const Home: NextPage = () => {
-  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
-
   return (
     <>
       <Head>
@@ -24,7 +21,7 @@ const Home: NextPage = () => {
           Create <span className="text-purple-300">T3</span> App
         </h1>
         <p className="text-2xl text-gray-700">This stack uses:</p>
-        <div className="grid gap-3 pt-3 mt-3 text-center md:grid-cols-2 lg:w-2/3">
+        <div className="grid gap-3 pt-3 mt-3 text-center md:grid-cols-3 lg:w-2/3">
           <TechnologyCard
             name="NextJS"
             description="The React framework for production"
@@ -40,14 +37,6 @@ const Home: NextPage = () => {
             description="Rapidly build modern websites without ever leaving your HTML"
             documentation="https://tailwindcss.com/"
           />
-          <TechnologyCard
-            name="tRPC"
-            description="End-to-end typesafe APIs made easy"
-            documentation="https://trpc.io/"
-          />
-        </div>
-        <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
-          {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
         </div>
       </main>
     </>
@@ -58,21 +47,19 @@ const TechnologyCard = ({
   name,
   description,
   documentation,
-}: TechnologyCardProps) => {
-  return (
-    <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
-      <h2 className="text-lg text-gray-700">{name}</h2>
-      <p className="text-sm text-gray-600">{description}</p>
-      <a
-        className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
-        href={documentation}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Documentation
-      </a>
-    </section>
-  );
-};
+}: TechnologyCardProps) => (
+  <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
+    <h2 className="text-lg text-gray-700">{name}</h2>
+    <p className="text-sm text-gray-600">{description}</p>
+    <a
+      className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
+      href={documentation}
+      target="_blank"
+      rel="noreferrer"
+    >
+      Documentation
+    </a>
+  </section>
+);
 
 export default Home;
