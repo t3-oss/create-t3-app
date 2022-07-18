e9024a8bc6adc64fe0945be6706eba1f84d92553
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2022-07-18 18:27:46 -0400

    refactor: client env validation

--- template/base/src/env/server-env.mjs
+++ template/base/src/env/server-env.mjs
@@ -3,7 +3,8 @@
  * This file is included in `/next.config.mjs` which ensures the app isn't built with invalid env vars.
  * It has to be a `.mjs`-file to be imported there.
  */
- import { serverEnvSchema, clientEnvSchema } from "./env-schema.mjs";
+import { serverEnvSchema } from "./env-schema.mjs";
+import { env as clientEnv } from "./client-env.mjs";
 
  const formatErrors = (
    /** @type {import('zod').ZodFormattedError<Map<string,string>,string>} */
@@ -17,7 +18,6 @@
      .filter(Boolean);
  
  const _serverEnv = serverEnvSchema.safeParse(process.env);
- const _clientEnv = clientEnvSchema.safeParse(process.env);
  
  if (!_serverEnv.success) {
    console.error(
@@ -26,25 +26,5 @@
    );
    throw new Error("Invalid environment variables");
  }
- 
- if (!_clientEnv.success) {
- console.error(
-   "❌ Invalid environment variables:\n",
-   ...formatErrors(_clientEnv.error.format()),
- );
- throw new Error("Invalid environment variables");
- }
- 
- for (let key of Object.keys(_clientEnv.data)) {
-   if (!key.startsWith("NEXT_PUBLIC_")) {
-     console.warn(
-       '❌ Invalid public environment variable name:',
-       key,
-     );
- 
-     throw new Error("Invalid public environment variable name");
-   }
- }
- 
- export const env = {..._serverEnv.data, ..._clientEnv.data};
- 
+
+export const env = {..._serverEnv.data, ...clientEnv};
