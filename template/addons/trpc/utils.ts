f6bee8f616a706d6b66d9eb8bdcf1e73ecee4f51
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-07-18 23:56:47 +0200

    fix: add missing trpc import

--- template/addons/trpc/utils.ts
+++ template/addons/trpc/utils.ts
@@ -1,6 +1,7 @@
 // src/utils/trpc.ts
 import type { AppRouter } from "../server/router";
 import { createReactQueryHooks } from "@trpc/react";
+import type { inferProcedureOutput, inferProcedureInput } from "@trpc/server";
 
 export const trpc = createReactQueryHooks<AppRouter>();
 
