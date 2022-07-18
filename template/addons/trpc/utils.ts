1af94ee460a99b763303267a1cf9cd270666c5be
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2022-07-18 18:31:14 -0400

    refactor: run prettier

--- template/addons/trpc/utils.ts
+++ template/addons/trpc/utils.ts
@@ -3,7 +3,6 @@ import type { AppRouter } from "../server/router";
 import { createReactQueryHooks } from "@trpc/react";
 import type { inferProcedureOutput, inferProcedureInput } from "@trpc/server";
 
-
 export const trpc = createReactQueryHooks<AppRouter>();
 
 /**
