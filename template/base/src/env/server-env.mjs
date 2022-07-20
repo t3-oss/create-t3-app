21dafe22391598a5d0e5db59633e8cbcf9c6863f
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2022-07-20 09:25:36 -0400

    fix: validate public env vars are not in server schema

--- template/base/src/env/server-env.mjs
+++ template/base/src/env/server-env.mjs
@@ -16,4 +16,14 @@ if (!_serverEnv.success) {
   throw new Error("Invalid environment variables");
 }
 
+for (let key of Object.keys(_serverEnv.data)) {
+  if (key.startsWith("NEXT_PUBLIC_")) {
+    console.warn(
+      "❌ You are exposing a server-side env-variable:", key
+    );
+
+    throw new Error("You are exposing a server-side env-variable");
+  }
+}
+
 export const env = {..._serverEnv.data, ...clientEnv};
