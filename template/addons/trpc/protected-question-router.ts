827f0aff677591292376099caf3bfc30f64c3f06
Vikrant Bhat <vik.bhat223@gmail.com>
2022-07-22 23:33:01 +0530

    chore: add getSession query

--- template/addons/trpc/protected-question-router.ts
+++ template/addons/trpc/protected-question-router.ts
@@ -1,14 +1,15 @@
 import { createProtectedRouter } from "./protected-router";
 
-export const protectedQuestionRouter = createProtectedRouter().query(
-  "get-my-questions",
-  {
+export const protectedQuestionRouter = createProtectedRouter()
+  .query("getSession", {
+    resolve({ ctx }) {
+      return ctx.session;
+    },
+  })
+  .query("getSecretMessage", {
     async resolve({ ctx }) {
       // Fetch protected data from your Database here and return
 
-      return [
-        "He who asks a question is a fool for five minutes; he who does not ask a question remains a fool forever.",
-      ];
+      return "He who asks a question is a fool for five minutes; he who does not ask a question remains a fool forever.";
     },
-  },
-);
+  });
