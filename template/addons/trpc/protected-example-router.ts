4ad5c934ec8680f58ce38417714417a39ab1712c
Vikrant Bhat <50735025+bhatvikrant@users.noreply.github.com>
2022-07-23 03:10:30 +0530

    chore: add protected trpc router example (#194)

---
+++ template/addons/trpc/protected-example-router.ts
@@ -0,0 +1,14 @@
+import { createProtectedRouter } from "./protected-router";
+
+// Example router with queries that can only be hit if the user requesting is signed in
+export const protectedExampleRouter = createProtectedRouter()
+  .query("getSession", {
+    resolve({ ctx }) {
+      return ctx.session;
+    },
+  })
+  .query("getSecretMessage", {
+    resolve({ ctx }) {
+      return "He who asks a question is a fool for five minutes; he who does not ask a question remains a fool forever.";
+    },
+  });
