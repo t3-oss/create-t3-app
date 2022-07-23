c50e4bc97940639a746c8759819a15ac36efadc3
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-07-23 14:56:26 +0200

    chore: remove credentials provider (#246)

--- template/addons/next-auth/api-handler-prisma.ts
+++ template/addons/next-auth/api-handler-prisma.ts
@@ -1,6 +1,5 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
-import CredentialsProvider from "next-auth/providers/credentials";
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
@@ -25,20 +24,6 @@ export const authOptions: NextAuthOptions = {
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
     // ...add more providers here
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        name: {
-          label: "Name",
-          type: "text",
-          placeholder: "Enter your name",
-        },
-      },
-      async authorize(credentials, _req) {
-        const user = { id: 1, name: credentials?.name ?? "J Smith" };
-        return user;
-      },
-    }),
   ],
 };
 
