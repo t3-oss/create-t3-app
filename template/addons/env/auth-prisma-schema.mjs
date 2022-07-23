ab03ce22c2cb17ace7a9f257eb5cf3d8cb5ec2ba
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-07-23 15:08:41 +0200

    fix: forgotten rename

--- template/addons/env/auth-prisma-schema.mjs
+++ template/addons/env/auth-prisma-schema.mjs
@@ -5,7 +5,7 @@ import { z } from "zod";
  * Specify your server-side environment variables schema here.
  * This way you can ensure the app isn't built with invalid env vars.
  */
-export const serverEnvSchema = z.object({
+export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
   NEXTAUTH_SECRET: z.string(),
--- template/addons/env/auth-schema.mjs
+++ template/addons/env/auth-schema.mjs
@@ -5,7 +5,7 @@ import { z } from "zod";
  * Specify your server-side environment variables schema here.
  * This way you can ensure the app isn't built with invalid env vars.
  */
-export const serverEnvSchema = z.object({
+export const serverSchema = z.object({
   NEXTAUTH_SECRET: z.string(),
   NEXTAUTH_URL: z.string().url(),
   DISCORD_CLIENT_ID: z.string(),
