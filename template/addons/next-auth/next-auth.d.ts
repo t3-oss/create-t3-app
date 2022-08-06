7627d2292c30d8c3027d4e21c530cb6f9de6bf71
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-08-06 07:01:24 +0200

    feat: make session.user.id nullable (next) (#284)

--- template/addons/next-auth/next-auth.d.ts
+++ template/addons/next-auth/next-auth.d.ts
@@ -5,7 +5,7 @@ declare module "next-auth" {
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
-    user?: {
+    user: {
       id?: string;
     } & DefaultSession["user"];
   }
