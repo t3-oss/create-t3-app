bbdcf183c7108c8748fb18bd3dd0cea121422226
Henrique <henriquesg09@hotmail.com>
2022-08-07 19:39:56 -0300

    feat(next-auth): add getServerAuthSession on auth-prisma-context file

--- template/addons/trpc/auth-prisma-context.ts
+++ template/addons/trpc/auth-prisma-context.ts
@@ -1,19 +1,17 @@
 // src/server/router/context.ts
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
-import { unstable_getServerSession as getServerSession } from "next-auth";
+import { getServerAuthSession } from "../common/get-server-auth-session";
 
-import { authOptions as nextAuthOptions } from "../../pages/api/auth/[...nextauth]";
 import { prisma } from "../db/client";
 
 export const createContext = async (
   opts?: trpcNext.CreateNextContextOptions,
 ) => {
-  const req = opts?.req;
-  const res = opts?.res;
+  const req = opts?.req as trpcNext.CreateNextContextOptions["req"];
+  const res = opts?.res as trpcNext.CreateNextContextOptions["res"];
 
-  const session =
-    req && res && (await getServerSession(req, res, nextAuthOptions));
+  const session = await getServerAuthSession({ req, res });
 
   return {
     req,
