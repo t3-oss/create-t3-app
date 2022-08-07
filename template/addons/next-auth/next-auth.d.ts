54b45e6d76cc6fb0c7ecc8e8e9c9f3c83a161afb
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-08-07 11:13:54 +0200

    fix: inverse the optional

--- template/addons/next-auth/next-auth.d.ts
+++ template/addons/next-auth/next-auth.d.ts
@@ -5,8 +5,8 @@ declare module "next-auth" {
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
-    user: {
-      id?: string;
+    user?: {
+      id: string;
     } & DefaultSession["user"];
   }
 }
