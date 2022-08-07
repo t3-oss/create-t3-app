e9f6e434e3316071eadcd63d418e5ad031bfd9f9
Henrique <henriquesg09@hotmail.com>
2022-08-07 14:56:51 -0300

    feat(next-auth): add get-server-auth-session file to next-auth installer

---
+++ template/addons/next-auth/get-server-auth-session.ts
@@ -0,0 +1,10 @@
+import { GetServerSidePropsContext } from "next";
+import { unstable_getServerSession } from "next-auth";
+import { authOptions as nextAuthOptions } from "../../pages/api/auth/[...nextauth]";
+
+export const getServerAuthSession = async (ctx: {
+  req: GetServerSidePropsContext["req"];
+  res: GetServerSidePropsContext["res"];
+}) => {
+  return await unstable_getServerSession(ctx.req, ctx.res, nextAuthOptions);
+};
