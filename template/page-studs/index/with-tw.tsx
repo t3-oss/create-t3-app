860fa99fc5b7dddf0a7263fe4180ee8b8b8a9edb
Jean-Pierre Stopinski <j@stopinski.at>
2022-08-07 12:44:21 +0200

    Add component to example

--- template/page-studs/index/with-tw.tsx
+++ template/page-studs/index/with-tw.tsx
@@ -29,9 +29,9 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
-        <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
-          Create <span className="text-purple-300">T3</span> App
-        </h1>
+        <Headline>
+          Create <span className={colors["purple"]}>T3</span> App
+        </Headline>
         <p className="text-2xl text-gray-700">This stack uses:</p>
         <div className="grid gap-3 pt-3 mt-3 text-center md:grid-cols-3 lg:w-2/3">
           <TechnologyCard
