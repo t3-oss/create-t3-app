4010904fb14143b648b0f7e1e648fb14d7f0b70d
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-08-15 20:36:57 +0200

    refactor: make monorepo

--- docs/README.md
+++
@@ -1,1 +0,0 @@
-# [create-t3-app](https://github.com/t3-oss/create-t3-app): Docs Site
---
+++ www/astro.config.mjs
@@ -0,0 +1,8 @@
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react()],
+});
