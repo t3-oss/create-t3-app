ad539118fdd93ed80774c2662da4a4eaabb08222
Julius Marminge <julius0216@outlook.com>
2022-08-15 14:56:12 +0200

    chore: Publish Workflow and other Github enhancements (#308)
    * chore: switch standard-version to changeset
    
    * chore: add version workflow
    
    * chore: issue forms and pr labeler
    
    * chore: prettier
    
    * chore: add missing credit
    
    * chore: adjust some commands
    
    * chore: update contributing guidelines
    
    * chore: prettier
    
    * chore: adjustments
    
    * feat: add minimal ts-linter
    
    * fix: lint warnings
    
    * revert eslinter

--- template/page-studs/_app/with-trpc.tsx
+++ template/page-studs/_app/with-trpc.tsx
@@ -10,17 +10,13 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 };
 
 const getBaseUrl = () => {
-  if (typeof window !== "undefined") {
-    return "";
-  }
-  if (process.browser) return ""; // Browser should use current path
+  if (typeof window !== undefined) return ""; // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
 export default withTRPC<AppRouter>({
-  config({ ctx }) {
+  config() {
     /**
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
