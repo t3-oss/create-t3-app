20b3eda7abed171cbcaf6214a64c62c55e307626
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-08-15 14:53:20 +0200

    revert eslinter

--- template/addons/prisma/client.ts
+++ template/addons/prisma/client.ts
@@ -3,7 +3,6 @@ import { PrismaClient } from "@prisma/client";
 import { env } from "../../env/server.mjs";
 
 declare global {
-  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
