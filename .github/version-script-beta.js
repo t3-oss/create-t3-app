60c70e8f58aad5165d64b06aa1e84e8a54b56bdc
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-08-15 15:46:39 +0200

    chore(ci): update versioning scripts

--- .github/version-script-beta.js
+++ .github/version-script-beta.js
@@ -2,7 +2,6 @@
 // https://github.com/cloudflare/wrangler2/blob/main/.github/version-script.js
 import fs from "fs";
 import { exec } from "child_process";
-import { getT3Version } from "../src/utils/getT3Version";
 
 try {
   const pkg = JSON.parse(fs.readFileSync("package.json"));
--- .github/version-script-next.js
+++ .github/version-script-next.js
@@ -2,7 +2,6 @@
 // https://github.com/cloudflare/wrangler2/blob/main/.github/version-script.js
 import fs from "fs";
 import { exec } from "child_process";
-import { getT3Version } from "../src/utils/getT3Version";
 
 try {
   const pkg = JSON.parse(fs.readFileSync("package.json"));
@@ -11,7 +10,6 @@ try {
       console.log(err);
       process.exit(1);
     }
-    const currentVersion = getT3Version();
     pkg.version = "6.0.0-next." + stdout.trim();
     fs.writeFileSync("package.json", JSON.stringify(pkg, null, "\t") + "\n");
   });
