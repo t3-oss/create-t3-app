67bf7e53db9ca347e62672ae5b80f38a8ad5cda8
Julius Marminge <julius0216@outlook.com>
2022-08-15 14:56:12 +0200

    chore: Publish Workflow and other Github enhancements (#308)
    * chore: switch standard-version to changeset
    
    * chore: add version workflow
    
    * chore: issue forms and pr labeler
    
    * chore: prettier
    
    * chore: add missing credit
    
    * chore: adjust some commands
    
    * chore: update contributing guidelines
    
    * chore: prettier
    
    * chore: adjustments
    
    * feat: add minimal ts-linter
    
    * fix: lint warnings
    
    * revert eslinter

---
+++ .github/version-script.js
@@ -0,0 +1,24 @@
+// ORIGINALLY FROM CLOUDFLARE WRANGLER:
+// https://github.com/cloudflare/wrangler2/blob/main/.github/version-script.js
+
+const fs = require("fs");
+const { exec } = require("child_process");
+
+try {
+  const package = JSON.parse(fs.readFileSync("package.json"));
+  exec("git rev-parse --short HEAD", (err, stdout) => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+    // Version has to supersede the currently available version
+    package.version = "5.5.0-" + stdout.trim();
+    fs.writeFileSync(
+      "package.json",
+      JSON.stringify(package, null, "\t") + "\n",
+    );
+  });
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
