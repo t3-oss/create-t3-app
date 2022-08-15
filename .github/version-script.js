6b4d4156c7d1394dcf6f78819e375dcca8129267
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-08-15 15:03:03 +0200

    chore(ci): dont use reserved word `package`

--- .github/version-script.js
+++ .github/version-script.js
@@ -1,22 +1,18 @@
 // ORIGINALLY FROM CLOUDFLARE WRANGLER:
 // https://github.com/cloudflare/wrangler2/blob/main/.github/version-script.js
-
 const fs = require("fs");
 const { exec } = require("child_process");
 
 try {
-  const package = JSON.parse(fs.readFileSync("package.json"));
+  const pkg = JSON.parse(fs.readFileSync("package.json"));
   exec("git rev-parse --short HEAD", (err, stdout) => {
     if (err) {
       console.log(err);
       process.exit(1);
     }
     // Version has to supersede the currently available version
-    package.version = "5.5.0-" + stdout.trim();
-    fs.writeFileSync(
-      "package.json",
-      JSON.stringify(package, null, "\t") + "\n",
-    );
+    pkg.version = "5.5.0-" + stdout.trim();
+    fs.writeFileSync("package.json", JSON.stringify(pkg, null, "\t") + "\n");
   });
 } catch (error) {
   console.error(error);
