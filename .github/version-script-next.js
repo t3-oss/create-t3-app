e5e3fca98ff7bf37c1ff959296fa7c374dac39c5
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-08-15 15:41:06 +0200

    chore(ci): next-release

---
+++ .github/version-script-next.js
@@ -0,0 +1,21 @@
+// ORIGINALLY FROM CLOUDFLARE WRANGLER:
+// https://github.com/cloudflare/wrangler2/blob/main/.github/version-script.js
+import fs from "fs";
+import { exec } from "child_process";
+import { getT3Version } from "../src/utils/getT3Version";
+
+try {
+  const pkg = JSON.parse(fs.readFileSync("package.json"));
+  exec("git rev-parse --short HEAD", (err, stdout) => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+    const currentVersion = getT3Version();
+    pkg.version = currentVersion + "-next." + stdout.trim();
+    fs.writeFileSync("package.json", JSON.stringify(pkg, null, "\t") + "\n");
+  });
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
