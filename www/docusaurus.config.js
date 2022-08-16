3f048c3c854ed7e6b06a9c0d97b651fb7c46136f
Shoubhit Dash <shoubhit2005@gmail.com>
2022-08-16 22:43:03 +0530

    docs: dark theme by default

--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -38,6 +38,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     {
+      colorMode: {
+        defaultMode: "dark",
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
       navbar: {
         title: "Create T3 App",
         logo: {
