3962cc211325fd3388806e8ec54e2abbb8c170d5
Julius Marminge <julius0216@outlook.com>
2022-08-17 13:06:55 +0200

    feat: make context independent of req/res (#325)

--- cli/template/addons/trpc/base-context.ts
+++ cli/template/addons/trpc/base-context.ts
@@ -2,8 +2,24 @@
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 
-export const createContext = (opts: trpcNext.CreateNextContextOptions) => {
+type CreateContextOptions = {};
+
+/** Use this helper for:
+ * - testing, where we dont have to Mock Next.js' req/res
+ * - trpc's `createSSGHelpers` where we don't have req/res
+ **/
+export const createContextInner = async (opts: CreateContextOptions) => {
   return {};
+};
+
+/**
+ * This is the actual context you'll use in your router
+ * @link https://trpc.io/docs/context
+ **/
+export const createContext = async (
+  opts: trpcNext.CreateNextContextOptions,
+) => {
+  return await createContextInner({});
 };
 
 export type Context = trpc.inferAsyncReturnType<typeof createContext>;
