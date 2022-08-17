4298645c73fb48e8967f89689ec26b8c2c7277a0
Julius Marminge <julius0216@outlook.com>
2022-08-17 13:06:55 +0200

    feat: make context independent of req/res (#325)

--- cli/template/addons/trpc/prisma-context.ts
+++ cli/template/addons/trpc/prisma-context.ts
@@ -3,10 +3,26 @@ import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 import { prisma } from "../db/client";
 
-export const createContext = (opts: trpcNext.CreateNextContextOptions) => {
+type CreateContextOptions = {};
+
+/** Use this helper for:
+ *  - testing, where we dont have to Mock Next.js' req/res
+ *  - trpc's `createSSGHelpers` where we don't have req/res
+ */
+export const createContextInner = async (opts: CreateContextOptions) => {
   return {
     prisma,
   };
+};
+
+/**
+ * This is the actual context you'll use in your router
+ * @link https://trpc.io/docs/context
+ **/
+export const createContext = async (
+  opts: trpcNext.CreateNextContextOptions,
+) => {
+  return await createContextInner({});
 };
 
 export type Context = trpc.inferAsyncReturnType<typeof createContext>;
