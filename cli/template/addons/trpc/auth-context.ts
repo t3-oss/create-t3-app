55a0f936a0937c980ae17453423e8eec6c4b4673
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-08-17 13:01:29 +0200

    feat: make context independent of req/res

--- cli/template/addons/trpc/auth-context.ts
+++ cli/template/addons/trpc/auth-context.ts
@@ -1,18 +1,38 @@
-// src/server/trpc/context.ts
+// src/server/router/context.ts
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
-import { unstable_getServerSession as getServerSession } from "next-auth";
-
+import {
+  Session,
+  unstable_getServerSession as getServerSession,
+} from "next-auth";
 import { authOptions as nextAuthOptions } from "../../pages/api/auth/[...nextauth]";
 
+type CreateContextOptions = {
+  session: Session | null;
+};
+
+/** Use this helper for:
+ *  - testing, where we dont have to Mock Next.js' req/res
+ *  - trpc's `createSSGHelpers` where we don't have req/res
+ **/
+export const createContextInner = async (opts: CreateContextOptions) => {
+  return {
+    session: opts.session,
+  };
+};
+
+/**
+ * This is the actual context you'll use in your router
+ * @link https://trpc.io/docs/context
+ **/
 export const createContext = async (
   opts: trpcNext.CreateNextContextOptions,
 ) => {
   const session = await getServerSession(opts.req, opts.res, nextAuthOptions);
 
-  return {
+  return await createContextInner({
     session,
-  };
+  });
 };
 
 export type Context = trpc.inferAsyncReturnType<typeof createContext>;
--- cli/template/addons/trpc/auth-prisma-context.ts
+++ cli/template/addons/trpc/auth-prisma-context.ts
@@ -1,20 +1,40 @@
-// src/server/trpc/context.ts
+// src/server/router/context.ts
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
-import { unstable_getServerSession as getServerSession } from "next-auth";
-
+import {
+  Session,
+  unstable_getServerSession as getServerSession,
+} from "next-auth";
 import { authOptions as nextAuthOptions } from "../../pages/api/auth/[...nextauth]";
 import { prisma } from "../db/client";
 
+type CreateContextOptions = {
+  session: Session | null;
+};
+
+/** Use this helper for:
+ * - testing, so we dont have to mock Next.js' req/res
+ * - trpc's `createSSGHelpers` where we don't have req/res
+ **/
+export const createContextInner = async (opts: CreateContextOptions) => {
+  return {
+    session: opts.session,
+    prisma,
+  };
+};
+
+/**
+ * This is the actual context you'll use in your router
+ * @link https://trpc.io/docs/context
+ **/
 export const createContext = async (
   opts: trpcNext.CreateNextContextOptions,
 ) => {
   const session = await getServerSession(opts.req, opts.res, nextAuthOptions);
 
-  return {
+  return await createContextInner({
     session,
-    prisma,
-  };
+  });
 };
 
 export type Context = trpc.inferAsyncReturnType<typeof createContext>;
