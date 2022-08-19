f02ee2170e510a0256f99f3f99eaa23084c4f15d
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-08-19 15:01:27 +0200

    fix: build fails on trpc, trpc+prisma (next) (#337)

--- cli/template/addons/trpc/base-context.ts
+++ cli/template/addons/trpc/base-context.ts
@@ -2,7 +2,10 @@
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 
-type CreateContextOptions = {};
+/**
+ * Replace this with an object if you want to pass things to createContextInner
+ */
+type CreateContextOptions = Record<string, never>;
 
 /** Use this helper for:
  * - testing, where we dont have to Mock Next.js' req/res
--- cli/template/addons/trpc/prisma-context.ts
+++ cli/template/addons/trpc/prisma-context.ts
@@ -3,7 +3,10 @@ import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 import { prisma } from "../db/client";
 
-type CreateContextOptions = {};
+/**
+ * Replace this with an object if you want to pass things to createContextInner
+ */
+type CreateContextOptions = Record<string, never>;
 
 /** Use this helper for:
  *  - testing, where we dont have to Mock Next.js' req/res
