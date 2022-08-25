284002e45d9a9c2bed620ca69916ef610e3e0ed8
sir-mr-bean <kroucher.1019@gmail.com>
2022-08-26 07:45:27 +1000

    feat: add robotstxt

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,16 +1,28 @@
 import { defineConfig } from "astro/config";
+//@ts-lint-ignore
+
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
+
 export default defineConfig({
+  site: `https://create.t3.gg`,
   integrations: [
     preact(), // Enable React for the Algolia search component.
     react(),
     tailwind(),
     image(),
+    robotsTxt({
+      transform(content) {
+        return `# Algolia-Crawler-Verif: 013A1A14AB9EE32B\n\n${content}`;
+      },
+    }),
   ],
-  site: `https://create.t3.gg`,
+  experimental: {
+    integrations: true,
+  },
 });
