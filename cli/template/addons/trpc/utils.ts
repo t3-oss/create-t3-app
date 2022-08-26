7689dd7f8e2f8abb2c1e3659eefa3aea5e0783c3
Julius Marminge <julius0216@outlook.com>
2022-08-26 10:00:57 +0200

    feat: add trpc loggerlink for dev (#352)

--- cli/template/addons/trpc/utils.ts
+++ cli/template/addons/trpc/utils.ts
@@ -1,4 +1,5 @@
 // src/utils/trpc.ts
+import { httpBatchLink, loggerLink } from "@trpc/client";
 import { setupTRPC } from "@trpc/next";
 import type { AppRouter } from "../server/trpc/router";
 import superjson from "superjson";
@@ -6,15 +7,23 @@ import superjson from "superjson";
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
 export const trpc = setupTRPC<AppRouter>({
   config() {
     return {
-      url: `${getBaseUrl()}/api/trpc`,
       transformer: superjson,
+      links: [
+        loggerLink({
+          enabled: (opts) =>
+            process.env.NODE_ENV === "development" ||
+            (opts.direction === "down" && opts.result instanceof Error),
+        }),
+        httpBatchLink({
+          url: `${getBaseUrl()}/api/trpc`,
+        }),
+      ],
     };
   },
   ssr: false,
