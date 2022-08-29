0f10684498efaca3d39e4adfb9e8c59a9d900053
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-08-29 10:52:01 +0200

    fix: take ownership of robots.txt

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -2,23 +2,11 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import robotsTxt from "astro-robots-txt";
 
 /**
  * @link https://astro.build/config
  */
 export default defineConfig({
   site: `https://create.t3.gg`,
-  integrations: [
-    react(),
-    tailwind(),
-    robotsTxt({
-      transform(content) {
-        return `# Algolia-Crawler-Verif: 013A1A14AB9EE32B\n\n${content}`;
-      },
-    }),
-  ],
-  experimental: {
-    integrations: true,
-  },
+  integrations: [react(), tailwind()],
 });
