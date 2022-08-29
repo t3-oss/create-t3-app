ebae5e2c538f1efa2ac703ccb83c96dfd3980566
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-08-29 21:42:29 +0200

    feat: add abort/clear/overwrite options on existing dir

--- cli/src/helpers/scaffoldProject.ts
+++ cli/src/helpers/scaffoldProject.ts
@@ -32,20 +32,35 @@ export const scaffoldProject = async ({
       );
     } else {
       spinner.stopAndPersist();
-      const { overwriteDir } = await inquirer.prompt<{ overwriteDir: boolean }>(
-        {
-          name: "overwriteDir",
-          type: "confirm",
-          message: `${chalk.redBright.bold("Warning:")} ${chalk.cyan.bold(
-            projectName,
-          )} already exists and isn't empty. Do you want to overwrite it?`,
-          default: false,
-        },
-      );
-      if (!overwriteDir) {
+      const { overwriteDir } = await inquirer.prompt<{
+        overwriteDir: "abort" | "clear" | "overwrite";
+      }>({
+        name: "overwriteDir",
+        type: "list",
+        message: `${chalk.redBright.bold("Warning:")} ${chalk.cyan.bold(
+          projectName,
+        )} already exists and isn't empty. How would you like to proceed?`,
+        choices: [
+          { name: "Abort installation", value: "abort", short: "Abort" },
+          {
+            name: "Clear the directory and continue installation",
+            value: "clear",
+            short: "Clear",
+          },
+          {
+            name: "Continue installation and overwrite conflicting files",
+            value: "overwrite",
+            short: "Overwrite",
+          },
+        ],
+        default: "abort",
+      });
+      if (overwriteDir === "abort") {
         spinner.fail("Aborting installation...");
         process.exit(0);
-      } else {
+      }
+
+      if (overwriteDir === "clear") {
         spinner.info(
           `Emptying ${chalk.cyan.bold(projectName)} and creating t3 app..\n`,
         );
