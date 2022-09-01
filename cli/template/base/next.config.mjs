ec122acb08af4572acf483b7365e6be967ee6656
vallerem <manuelvalle@vallerem.com>
2022-09-01 21:26:01 +0200

    feat: add basic i18n configuration in next.config and set english as the default lang

--- cli/template/base/next.config.mjs
+++ cli/template/base/next.config.mjs
@@ -15,4 +15,8 @@ function defineNextConfig(config) {
 export default defineNextConfig({
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
 });
