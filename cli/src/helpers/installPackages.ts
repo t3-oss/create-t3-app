ecd6ad1aa9f8639722e9c31f16017c2f65660319
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-09-02 21:22:32 +0200

    fix: remove unused import

--- cli/src/helpers/installPackages.ts
+++ cli/src/helpers/installPackages.ts
@@ -2,7 +2,6 @@ import type { InstallerOptions, PkgInstallerMap } from "../installers/index.js";
 import chalk from "chalk";
 import ora from "ora";
 import { logger } from "~/utils/logger.js";
-import { noTailwindInstaller } from "../installers/noTailwind.js";
 
 type InstallPackagesOptions = {
   packages: PkgInstallerMap;
