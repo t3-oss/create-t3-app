e38bf81f0dd95db21996c61b0b9f09f00ced8866
Balázs Orbán <info@balazsorban.com>
2022-09-09 12:39:49 +0200

    Update _app.tsx

--- cli/template/base/src/pages/_app.tsx
+++ cli/template/base/src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import "../styles/globals.css";
-import type { AppType } from "next/dist/shared/lib/utils";
+import type { AppProps } from "next/app";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 };
-
-export default MyApp;
