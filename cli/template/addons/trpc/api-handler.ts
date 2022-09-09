d0be78811fa51806d14239b230291adfc36c949a
Ted Månsken <ted.mansken@pulsen.se>
2022-09-09 15:14:44 +0200

    fix: use validated env-vars

--- cli/template/addons/trpc/api-handler.ts
+++ cli/template/addons/trpc/api-handler.ts
@@ -1,5 +1,6 @@
 // src/pages/api/trpc/[trpc].ts
 import { createNextApiHandler } from "@trpc/server/adapters/next";
+import { env } from "../../../env/server.mjs";
 import { appRouter } from "../../../server/router";
 import { createContext } from "../../../server/router/context";
 
@@ -8,7 +9,7 @@ export default createNextApiHandler({
   router: appRouter,
   createContext,
   onError:
-    process.env.NODE_ENV === "development"
+    env.NODE_ENV === "development"
       ? ({ path, error }) => {
           console.error(`❌ tRPC failed on ${path}: ${error}`);
         }
