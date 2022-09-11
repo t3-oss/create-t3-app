72c05a68e7e40b8578bccc7d58a82feac612529b
Alex <@.gg>
2022-09-10 21:29:50 -0300

    feat(template): removes duplicated code

--- cli/template/base/src/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import "../styles/globals.css";
-import type { AppType } from "next/dist/shared/lib/utils";
-
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
-
-export default MyApp;
