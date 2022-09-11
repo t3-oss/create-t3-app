36c3f2b3a02f420e82690d6c5763d0667a4bf896
ted-pulsen <109650792+ted-pulsen@users.noreply.github.com>
2022-09-11 10:04:57 +0200

    feat: error handler for trpc (#413)

--- cli/template/addons/trpc/api-handler.ts
+++ cli/template/addons/trpc/api-handler.ts
@@ -1,5 +1,6 @@
 // src/pages/api/trpc/[trpc].ts
 import { createNextApiHandler } from "@trpc/server/adapters/next";
+import { env } from "../../../env/server.mjs";
 import { appRouter } from "../../../server/router";
 import { createContext } from "../../../server/router/context";
 
@@ -7,4 +8,10 @@ import { createContext } from "../../../server/router/context";
 export default createNextApiHandler({
   router: appRouter,
   createContext,
+  onError:
+    env.NODE_ENV === "development"
+      ? ({ path, error }) => {
+          console.error(`❌ tRPC failed on ${path}: ${error}`);
+        }
+      : undefined,
 });
