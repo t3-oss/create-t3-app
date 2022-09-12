bf58aed63b542b6f88ddf102b449a785a217a465
Alex <@.gg>
2022-09-11 23:23:31 -0300

    feat(cli): scaffolds with `git` now

--- cli/src/helpers/scaffoldProject.ts
+++ cli/src/helpers/scaffoldProject.ts
@@ -1,11 +1,10 @@
-import path from "path";
 import chalk from "chalk";
 import fs from "fs-extra";
 import inquirer from "inquirer";
-import ora from "ora";
-import { PKG_ROOT } from "~/consts.js";
+import ora, { Ora } from "ora";
 import { InstallerOptions } from "~/installers/index.js";
 import { logger } from "~/utils/logger.js";
+import { cloneScaffoldAndReturnPath } from "~/utils/git.js";
 
 // This bootstraps the base Next.js application
 export const scaffoldProject = async ({
@@ -13,9 +12,9 @@ export const scaffoldProject = async ({
   projectDir,
   pkgManager,
   noInstall,
-}: InstallerOptions) => {
-  const srcDir = path.join(PKG_ROOT, "template/base");
-
+}: InstallerOptions & {
+  projectName: string;
+}) => {
   if (!noInstall) {
     logger.info(`\nUsing: ${chalk.cyan.bold(pkgManager)}\n`);
   } else {
@@ -24,80 +23,78 @@ export const scaffoldProject = async ({
 
   const spinner = ora(`Scaffolding in: ${projectDir}...\n`).start();
 
-  if (fs.existsSync(projectDir)) {
-    if (fs.readdirSync(projectDir).length === 0) {
-      spinner.info(
-        `${chalk.cyan.bold(projectName)} exists but is empty, continuing...\n`,
-      );
-    } else {
-      spinner.stopAndPersist();
-      const { overwriteDir } = await inquirer.prompt<{
-        overwriteDir: "abort" | "clear" | "overwrite";
-      }>({
-        name: "overwriteDir",
-        type: "list",
-        message: `${chalk.redBright.bold("Warning:")} ${chalk.cyan.bold(
-          projectName,
-        )} already exists and isn't empty. How would you like to proceed?`,
-        choices: [
-          {
-            name: "Abort installation (recommended)",
-            value: "abort",
-            short: "Abort",
-          },
-          {
-            name: "Clear the directory and continue installation",
-            value: "clear",
-            short: "Clear",
-          },
-          {
-            name: "Continue installation and overwrite conflicting files",
-            value: "overwrite",
-            short: "Overwrite",
-          },
-        ],
-        default: "abort",
-      });
-      if (overwriteDir === "abort") {
-        spinner.fail("Aborting installation...");
-        process.exit(0);
-      }
-
-      const overwriteAction =
-        overwriteDir === "clear"
-          ? "clear the directory"
-          : "overwrite conflicting files";
-
-      const { confirmOverwriteDir } = await inquirer.prompt<{
-        confirmOverwriteDir: boolean;
-      }>({
-        name: "confirmOverwriteDir",
-        type: "confirm",
-        message: `Are you sure you want to ${overwriteAction}?`,
-        default: false,
-      });
-
-      if (!confirmOverwriteDir) {
-        spinner.fail("Aborting installation...");
-        process.exit(0);
-      }
-
-      if (overwriteDir === "clear") {
-        spinner.info(
-          `Emptying ${chalk.cyan.bold(projectName)} and creating t3 app..\n`,
-        );
-        fs.emptyDirSync(projectDir);
-      }
-    }
-  }
-
-  spinner.start();
-
-  fs.copySync(srcDir, projectDir);
-  fs.renameSync(
-    path.join(projectDir, "_gitignore"),
-    path.join(projectDir, ".gitignore"),
-  );
+  await assertProjectDirIsntDirty(projectName, projectDir, spinner);
+  await cloneScaffoldAndReturnPath(projectName);
 
   spinner.succeed(`${chalk.cyan.bold(projectName)} scaffolded successfully!\n`);
+};
+
+const assertProjectDirIsntDirty = async (
+  projectName: string,
+  projectDir: string,
+  spinner: Ora,
+) => {
+  if (!fs.existsSync(projectDir)) return;
+
+  if (fs.readdirSync(projectDir).length === 0) {
+    spinner.info(
+      `${chalk.cyan.bold(projectName)} exists but is empty, continuing...\n`,
+    );
+
+    return;
+  }
+
+  spinner.stopAndPersist();
+
+  const overwriteDir = await promptClean(projectName);
+
+  if (overwriteDir === "abort" || !(await promptConfirmClean())) {
+    spinner.fail("Aborting installation...");
+    process.exit(0);
+  }
+
+  spinner.info(
+    `Emptying ${chalk.cyan.bold(projectName)} and creating t3 app..\n`,
+  );
+  fs.emptyDirSync(projectDir);
+};
+
+const promptClean = async (projectName: string) => {
+  const { clean } = await inquirer.prompt<{
+    clean: "abort" | "clear";
+  }>({
+    name: "clean",
+    type: "list",
+    message: `${chalk.redBright.bold("Warning:")} ${chalk.cyan.bold(
+      projectName,
+    )} already exists and isn't empty. How would you like to proceed?`,
+    choices: [
+      {
+        name: "Abort installation (recommended)",
+        value: "abort",
+        short: "Abort",
+      },
+      {
+        name: "Clear the directory and continue installation",
+        value: "clear",
+        short: "Clear",
+      },
+    ],
+    default: "abort",
+  });
+
+  return clean;
+};
+
+const promptConfirmClean = async (): Promise<boolean> => {
+  const { confirmOverwriteDir } = await inquirer.prompt<{
+    confirmOverwriteDir: boolean;
+  }>({
+    name: "confirmOverwriteDir",
+    type: "confirm",
+    message: `Are you sure you want to clear the directory?`,
+    default: false,
+  });
+
+  return confirmOverwriteDir;
 };
