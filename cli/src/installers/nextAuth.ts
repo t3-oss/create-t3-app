4ac84627a99a9531751c00928565209a25446a0c
Alex <@.gg>
2022-09-13 11:33:03 -0300

    feat(cli/patches): adds `nextAuth`

--- cli/src/installers/nextAuth.ts
+++ cli/src/installers/nextAuth.ts
@@ -1,18 +1,24 @@
 import type { Installer } from "~/installers/index.js";
-import { applyPatch, generatePatches } from "~/utils/git.js";
+import { deletePatches, generatePatches } from "~/utils/git.js";
 import fs from "fs-extra";
+import { Patch } from "~/utils/patch.js";
 
 export const nextAuthInstaller: Installer = async ({
   projectDir,
+  packagesInUse,
 }): Promise<void> => {
   await generatePatches("nextAuth", projectDir);
-
   const patchesFolder = `${projectDir}/patches`;
-  const patches = fs.readdirSync(patchesFolder);
 
   // We can apply every patch on nextAuth because they don't conflict with anything.
   // (other packages that conflict with nextAuth)
+  const patches = fs
+    .readdirSync(patchesFolder)
+    .map((file) => new Patch(file, "nextAuth"));
+
   for (const patch of patches) {
-    await applyPatch(projectDir, patch);
+    await patch.apply(projectDir);
   }
+
+  await deletePatches(projectDir);
 };
