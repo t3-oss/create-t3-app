a399cc26ae35b15fcd5d04a94e6e4fa413d529e3
Alex <@.gg>
2022-09-13 14:56:19 -0300

    feat(cli/patches): adds `tailwind`

--- cli/src/installers/tailwind.ts
+++ cli/src/installers/tailwind.ts
@@ -1,31 +1,24 @@
 import type { Installer } from "~/installers/index.js";
-import path from "path";
+import { deletePatches, generatePatches } from "~/utils/git.js";
 import fs from "fs-extra";
-import { PKG_ROOT } from "~/consts.js";
-import { addPackageDependency } from "~/utils/addPackageDependency.js";
+import { Patch } from "~/utils/patch.js";
 
-export const tailwindInstaller: Installer = ({ projectDir }) => {
-  addPackageDependency({
-    projectDir,
-    dependencies: ["tailwindcss", "postcss", "autoprefixer"],
-    devMode: true,
-  });
+export const tailwindInstaller: Installer = async ({
+  projectDir,
+  packagesInUse,
+}) => {
+  await generatePatches("tailwind", projectDir);
 
-  const twAssetDir = path.join(PKG_ROOT, "template/addons/tailwind");
+  const patchesFolder = `${projectDir}/patches`;
 
-  const twCfgSrc = path.join(twAssetDir, "tailwind.config.cjs");
-  const twCfgDest = path.join(projectDir, "tailwind.config.cjs");
+  const patches = fs
+    .readdirSync(patchesFolder)
+    .map((file) => new Patch(file, packagesInUse))
+    .filter((patch) => !patch.isBlocked());
 
-  const postcssCfgSrc = path.join(twAssetDir, "postcss.config.cjs");
-  const postcssCfgDest = path.join(projectDir, "postcss.config.cjs");
+  for (const patch of patches) {
+    await patch.apply(projectDir);
+  }
 
-  const cssSrc = path.join(twAssetDir, "globals.css");
-  const cssDest = path.join(projectDir, "src/styles/globals.css");
-
-  const indexModuleCss = path.join(projectDir, "src/pages/index.module.css");
-
-  fs.copySync(twCfgSrc, twCfgDest);
-  fs.copySync(postcssCfgSrc, postcssCfgDest);
-  fs.copySync(cssSrc, cssDest);
-  fs.unlinkSync(indexModuleCss);
+  await deletePatches(projectDir);
 };
