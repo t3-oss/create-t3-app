f03d03e5d316b8f2d75cf3009a78abacf40b7566
Gabriel De Andrade <30420087+gabrielelpidio@users.noreply.github.com>
2022-09-26 16:33:14 -0400

    feat(prettier): add prettier-plugin-tailwindcss to website and cli  (#481)
    * feat(prettier): add prettier-plugin-tailwindcss to website and cli generated code
    
    * feat(prettier): add prettier-plugin-tailwind to cli if tailwind is chosen
    
    * chore: add changeset
    
    * remove prettier from tailwind installer
    
    * add to config just to be sure
    
    * hmm?
    
    * prettier
    
    * revert test change
    
    Co-authored-by: Julius Marminge <julius0216@outlook.com>
    Co-authored-by: Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>

--- cli/template/page-studs/index/with-trpc-tw.tsx
+++ cli/template/page-studs/index/with-trpc-tw.tsx
@@ -13,12 +13,12 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
-        <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
+      <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
+        <h1 className="text-5xl font-extrabold leading-normal text-gray-700 md:text-[5rem]">
           Create <span className="text-purple-300">T3</span> App
         </h1>
         <p className="text-2xl text-gray-700">This stack uses:</p>
-        <div className="grid gap-3 pt-3 mt-3 text-center md:grid-cols-2 lg:w-2/3">
+        <div className="mt-3 grid gap-3 pt-3 text-center md:grid-cols-2 lg:w-2/3">
           <TechnologyCard
             name="NextJS"
             description="The React framework for production"
@@ -50,7 +50,7 @@ const Home: NextPage = () => {
             documentation="https://www.prisma.io/docs/"
           />
         </div>
-        <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
+        <div className="flex w-full items-center justify-center pt-6 text-2xl text-blue-500">
           {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
         </div>
       </main>
@@ -72,11 +72,11 @@ const TechnologyCard = ({
   documentation,
 }: TechnologyCardProps) => {
   return (
-    <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
+    <section className="flex flex-col justify-center rounded border-2 border-gray-500 p-6 shadow-xl duration-500 motion-safe:hover:scale-105">
       <h2 className="text-lg text-gray-700">{name}</h2>
       <p className="text-sm text-gray-600">{description}</p>
       <a
-        className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
+        className="mt-3 text-sm text-violet-500 underline decoration-dotted underline-offset-2"
         href={documentation}
         target="_blank"
         rel="noreferrer"
