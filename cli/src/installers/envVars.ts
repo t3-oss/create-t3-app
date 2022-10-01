4fb5e70717d67e2756be970b06afa53fcf7307e0
CDE90 <cdeyolo@outlook.com>
2022-10-01 19:59:07 +0100

    feat: tailor .env to only include relevant keys

--- cli/src/installers/envVars.ts
+++ cli/src/installers/envVars.ts
@@ -13,29 +13,33 @@ export const envVariablesInstaller: Installer = async ({
   const envAssetDir = path.join(PKG_ROOT, "template/addons/env");
 
   let envFile = "";
+  let envExample = "";
 
   switch (true) {
     case usingAuth && usingPrisma:
       envFile = "auth-prisma-schema.mjs";
+      envExample = "auth-prisma.env-example";
       break;
     case usingAuth:
       envFile = "auth-schema.mjs";
+      envExample = "auth.env-example";
       break;
     case usingPrisma:
       envFile = "prisma-schema.mjs";
+      envExample = "prisma.env-example";
       break;
   }
 
-  if (!envFile) return;
+  if (!envFile || !envExample) return;
 
   const envSchemaSrc = path.join(envAssetDir, envFile);
   const envSchemaDest = path.join(projectDir, "src/env/schema.mjs");
 
-  const envExampleSrc = path.join(projectDir, ".env-example");
+  const envExampleSrc = path.join(envAssetDir, envExample);
   const envDest = path.join(projectDir, ".env");
 
   await Promise.all([
     fs.copy(envSchemaSrc, envSchemaDest, { overwrite: true }),
-    fs.rename(envExampleSrc, envDest),
+    fs.copy(envExampleSrc, envDest, { overwrite: true }),
   ]);
 };
