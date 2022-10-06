5460add9ff6c1c61fd452c0d6245fb61cc6627f4
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-10-06 09:40:02 +0200

    update schema

--- cli/template/addons/env/auth-schema.mjs
+++ cli/template/addons/env/auth-schema.mjs
@@ -9,8 +9,10 @@ export const serverSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]),
   NEXTAUTH_SECRET: z.string(),
   NEXTAUTH_URL: z.preprocess(
+    // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+    // Since NextAuth automatically uses the VERCEL_URL if present.
     (str) => process.env.VERCEL_URL ?? str,
-    z.string().url(),
+    z.string(),
   ),
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
