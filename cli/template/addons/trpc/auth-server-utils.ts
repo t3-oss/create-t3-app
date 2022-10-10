dd0e172579d0436f5bdb5d29b721dd55362f620e
Julius Marminge <julius0216@outlook.com>
2022-10-10 23:55:09 +0200

    feat: align trpc usage to official docs (#581)

--- cli/template/addons/trpc/auth-server-utils.ts
+++ cli/template/addons/trpc/auth-server-utils.ts
@@ -2,22 +2,37 @@ import { initTRPC, TRPCError } from "@trpc/server";
 import type { Context } from "./context";
 import superjson from "superjson";
 
-export const t = initTRPC.context<Context>().create({
+const t = initTRPC.context<Context>().create({
   transformer: superjson,
   errorFormatter({ shape }) {
     return shape;
   },
 });
 
-export const authedProcedure = t.procedure.use(({ ctx, next }) => {
+export const router = t.router;
+
+/**
+ * Unprotected procedure
+ **/
+export const publicProcedure = t.procedure;
+
+/**
+ * Reusable middleware to ensure
+ * users are logged in
+ */
+const isAuthed = t.middleware(({ ctx, next }) => {
   if (!ctx.session || !ctx.session.user) {
     throw new TRPCError({ code: "UNAUTHORIZED" });
   }
   return next({
     ctx: {
-      ...ctx,
-      // infers that `session` is non-nullable to downstream resolvers
+      // infers the `session` as non-nullable
       session: { ...ctx.session, user: ctx.session.user },
     },
   });
 });
+
+/**
+ * Protected procedure
+ **/
+export const protectedProcedure = t.procedure.use(isAuthed);
--- cli/template/addons/trpc/example-prisma-router.ts
+++ cli/template/addons/trpc/example-prisma-router.ts
@@ -1,15 +1,15 @@
-import { t } from "../trpc";
+import { router, publicProcedure } from "../trpc";
 import { z } from "zod";
 
-export const exampleRouter = t.router({
-  hello: t.procedure
+export const exampleRouter = router({
+  hello: publicProcedure
     .input(z.object({ text: z.string().nullish() }).nullish())
     .query(({ input }) => {
       return {
         greeting: `Hello ${input?.text ?? "world"}`,
       };
     }),
-  getAll: t.procedure.query(({ ctx }) => {
+  getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.example.findMany();
   }),
 });
