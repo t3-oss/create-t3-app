4d3ba6d00b7a2ad6084809b3af09cd557cdb5abb
Shoubhit Dash <shoubhit2005@gmail.com>
2022-10-11 12:38:26 +0530

    feat(template): add prettier.Config type to prettier config and resolve plugin import

--- cli/template/addons/tailwind/prettier.config.cjs
+++ cli/template/addons/tailwind/prettier.config.cjs
@@ -1,3 +1,5 @@
+/** @type {import("prettier").Config} */
+
 module.exports = {
-  plugins: [require("prettier-plugin-tailwindcss")],
+  plugins: [require.resolve("prettier-plugin-tailwindcss")],
 };
