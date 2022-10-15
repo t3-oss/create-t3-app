47f23bccbcf0249e53f7960f396f02a642c6f6dd
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-10-15 13:03:08 +0200

    redundant variable declaration

--- cli/src/helpers/installDependencies.ts
+++ cli/src/helpers/installDependencies.ts
@@ -6,8 +6,7 @@ import { logger } from "~/utils/logger.js";
 export const installDependencies = async (projectDir: string) => {
   logger.info("Installing dependencies...");
   const pkgManager = getUserPkgManager();
-  const command = `${pkgManager} install`;
-  const spinner = ora(`Running ${command}...\n`).start();
+  const spinner = ora(`Running ${pkgManager} install...\n`).start();
 
   await execa(pkgManager, ["install"], { cwd: projectDir });
 
