8e07f56be0033470db338bfdb701fe1de0766f70
Shoubhit Dash <shoubhit2005@gmail.com>
2022-10-15 13:48:37 +0530

    fix(cli): make loader async to make it spin

--- cli/src/helpers/installDependencies.ts
+++ cli/src/helpers/installDependencies.ts
@@ -1,15 +1,15 @@
-import { execSync } from "child_process";
 import ora from "ora";
+import { execa } from "execa";
 import { getUserPkgManager } from "~/utils/getUserPkgManager.js";
 import { logger } from "~/utils/logger.js";
 
-export const installDependencies = (projectDir: string) => {
+export const installDependencies = async (projectDir: string) => {
   logger.info("Installing dependencies...");
   const pkgManager = getUserPkgManager();
   const command = `${pkgManager} install`;
   const spinner = ora(`Running ${command}...\n`).start();
 
-  execSync(command, { cwd: projectDir });
+  await execa(command, { cwd: projectDir });
 
   spinner.succeed("Successfully installed dependencies!\n");
 };
