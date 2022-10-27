6e1794492623dec83315663658548c91d7d88a22
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-10-27 10:25:06 +0200

    properly apply flags

--- cli/src/helpers/installDependencies.ts
+++ cli/src/helpers/installDependencies.ts
@@ -1,36 +1,47 @@
 import chalk from "chalk";
 import { execa } from "execa";
 import ora from "ora";
+import { PkgInstallerMap } from "~/installers/index.js";
 import { getUserPkgManager } from "~/utils/getUserPkgManager.js";
 import { logger } from "~/utils/logger.js";
 
-export const installDependencies = async (projectDir: string) => {
+type Options = {
+  packages: PkgInstallerMap;
+};
+
+export const installDependencies = async (
+  projectDir: string,
+  opts: Options,
+) => {
   logger.info("Installing dependencies...");
   const pkgManager = getUserPkgManager();
   const spinner = ora(`Running ${pkgManager} install...\n`).start();
+  let flags: string[] = [];
 
-  // FIXME: temp fix for next-auth with node 18
-  // see: https://github.com/nextauthjs/next-auth/issues/4575
   if (
     process.versions.node.startsWith("18") ||
     process.versions.node.startsWith("19")
   ) {
-    const engineFlag =
-      pkgManager === "yarn"
+    // FIXME: temp fix for next-auth with node 18/19
+    // see: https://github.com/nextauthjs/next-auth/issues/4575
+    flags = [
+      ...flags,
+      ...(pkgManager === "yarn"
         ? ["--ignore-engines", "true"]
-        : ["--engine-strict", "false"];
-    const peerDepFlag =
-      pkgManager === "pnpm"
-        ? ["--strict-peer-dependencies", "false"]
-        : /** npm uses strict-peer-deps=false by default, yarn unknown */
-          [];
-
-    await execa(pkgManager, ["install", ...engineFlag, ...peerDepFlag], {
-      cwd: projectDir,
-    });
-  } else {
-    await execa(pkgManager, ["install"], { cwd: projectDir });
+        : ["--engine-strict", "false"]),
+    ];
   }
+  if (opts.packages.nextAuth.inUse) {
+    // FIXME: temp fix for NextAuth.js with Next.js 13
+    flags = [
+      ...flags,
+      ...(pkgManager === "yarn"
+        ? ["--ignore-engines", "true"]
+        : ["--engine-strict", "false"]),
+    ];
+  }
+
+  await execa(pkgManager, ["install", ...flags], { cwd: projectDir });
 
   spinner.succeed(chalk.green("Successfully installed dependencies!\n"));
 };
