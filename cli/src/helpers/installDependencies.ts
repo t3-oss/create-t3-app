772aea5d11283937f0c1afc06ba9daf1a089317d
PossibleNPC <28771786+PossibleNPC@users.noreply.github.com>
2022-11-05 16:46:22 +0100

    fix(cli): fix create-t3-app to use yarn default behavior (#678) (#691)

--- cli/src/helpers/installDependencies.ts
+++ cli/src/helpers/installDependencies.ts
@@ -11,9 +11,17 @@ type Options = {
 export const installDependencies = async ({ projectDir }: Options) => {
   logger.info("Installing dependencies...");
   const pkgManager = getUserPkgManager();
-  const spinner = ora(`Running ${pkgManager} install...\n`).start();
+  const spinner =
+    pkgManager === "yarn"
+      ? ora("Running yarn...\n").start()
+      : ora(`Running ${pkgManager} install...\n`).start();
 
-  await execa(pkgManager, ["install"], { cwd: projectDir });
+  // If the package manager is yarn, use yarn's default behavior to install dependencies
+  if (pkgManager === "yarn") {
+    await execa(pkgManager, [], { cwd: projectDir });
+  } else {
+    await execa(pkgManager, ["install"], { cwd: projectDir });
+  }
 
   spinner.succeed(chalk.green("Successfully installed dependencies!\n"));
 };
