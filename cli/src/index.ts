38fcbaefbe139fbe030b3a250a59cbb206c2c486
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-11-13 12:09:18 +0100

    render warning when using a tagged or outdated version

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -10,6 +10,7 @@ import { buildPkgInstallerMap } from "~/installers/index.js";
 import { logger } from "~/utils/logger.js";
 import { parseNameAndPath } from "~/utils/parseNameAndPath.js";
 import { renderTitle } from "~/utils/renderTitle.js";
+import { renderVersionWarning } from "./utils/renderVersionWarning.js";
 import { installDependencies } from "./helpers/installDependencies.js";
 import { getVersion } from "./utils/getT3Version.js";
 
@@ -21,6 +22,7 @@ type CT3APackageJSON = PackageJson & {
 
 const main = async () => {
   renderTitle();
+  renderVersionWarning();
 
   const {
     appName,
---
+++ cli/src/utils/renderVersionWarning.ts
@@ -0,0 +1,30 @@
+import { execSync } from "child_process";
+import { getVersion } from "./getT3Version.js";
+import { logger } from "./logger.js";
+
+export const renderVersionWarning = () => {
+  const currentVersion = getVersion();
+  const npmVersion = execSync("npm view create-t3-app version")
+    .toString()
+    .trim();
+
+  if (currentVersion.includes("@beta")) {
+    logger.warn("You are using a beta version of create-t3-app.");
+    logger.warn("Please report any bugs you encounter.");
+  } else if (currentVersion.includes("@next")) {
+    logger.warn(
+      "You are running create-t3-app with the @next tag which is no longer maintained.",
+    );
+    logger.warn("Please run the CLI with @latest instead.");
+  } else if (currentVersion !== npmVersion) {
+    logger.warn("You are using an outdated version of create-t3-app.");
+    logger.warn(
+      "Your version:",
+      currentVersion + ".",
+      "Latest version in the npm registry:",
+      npmVersion,
+    );
+    logger.warn("Please run the CLI with @latest to get the latest updates.");
+  }
+  console.log("");
+};
