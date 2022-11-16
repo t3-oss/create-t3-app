a2cec4c0631dca5637fe1d9590f387ea2b16490a
Bruno Crosier <bruno.crosier@gmail.com>
2022-11-16 23:40:33 +0000

    fix: session type

--- cli/template/addons/next-auth/next-auth.d.ts
+++ cli/template/addons/next-auth/next-auth.d.ts
@@ -5,8 +5,6 @@ declare module "next-auth" {
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
-    user?: {
-      id: string;
-    } & DefaultSession["user"];
+    user?: DefaultSession["user"];
   }
 }
