d5d5a2f7503cf9d7c3bfab4609aeb6a0188d6af1
HatulaPro <70011806+HatulaPro@users.noreply.github.com>
2022-11-18 21:53:13 +0200

    fix: removing redundant optional chaining in templates (#800)

--- cli/template/page-studs/index/with-auth-trpc.tsx
+++ cli/template/page-studs/index/with-auth-trpc.tsx
@@ -68,7 +68,7 @@ const AuthShowcase: React.FC = () => {
   return (
     <div className={styles.authContainer}>
       <p className={styles.showcaseText}>
-        {sessionData && <span>Logged in as {sessionData?.user?.name}</span>}
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
         {secretMessage && <span> - {secretMessage}</span>}
       </p>
       <button
