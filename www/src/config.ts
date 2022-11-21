db864d571171e45a54530c170f7a0c8df1e7a114
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-11-21 11:35:35 +0000

    add ToC for in progress translations

--- www/src/config.ts
+++ www/src/config.ts
@@ -25,7 +25,11 @@ export type Frontmatter = {
 };
 
 export const KNOWN_LANGUAGES = {
+  Arabic: "ar",
   English: "en",
+  Polish: "pl",
+  Russian: "ru",
+  Spanish: "es",
 } as const;
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
@@ -45,6 +49,34 @@ export type Sidebar = Record<
   Record<string, { text: string; link: string }[]>
 >;
 export const SIDEBAR: Sidebar = {
+  ar: {
+    "Create T3 App": [
+      { text: "Introduction (Translate these!!)", link: "ar/introduction" },
+      { text: "Why CT3A?", link: "ar/why" },
+      { text: "Installation", link: "ar/installation" },
+      { text: "Folder Structure", link: "ar/folder-structure" },
+      { text: "FAQ", link: "ar/faq" },
+      { text: "T3 Collection", link: "ar/t3-collection" },
+      { text: "Other Recommendations", link: "ar/other-recs" },
+    ],
+    Usage: [
+      { text: "First Steps", link: "ar/usage/first-steps" },
+      { text: "Next.js", link: "ar/usage/next-js" },
+      { text: "TypeScript", link: "ar/usage/typescript" },
+      { text: "tRPC", link: "ar/usage/trpc" },
+      { text: "Prisma", link: "ar/usage/prisma" },
+      { text: "NextAuth.js", link: "ar/usage/next-auth" },
+      {
+        text: "Environment Variables",
+        link: "ar/usage/env-variables",
+      },
+      { text: "Tailwind CSS", link: "ar/usage/tailwind" },
+    ],
+    Deployment: [
+      { text: "Vercel", link: "ar/deployment/vercel" },
+      { text: "Docker", link: "ar/deployment/docker" },
+    ],
+  },
   en: {
     "Create T3 App": [
       { text: "Introduction", link: "en/introduction" },
@@ -71,6 +103,90 @@ export const SIDEBAR: Sidebar = {
     Deployment: [
       { text: "Vercel", link: "en/deployment/vercel" },
       { text: "Docker", link: "en/deployment/docker" },
+    ],
+  },
+  es: {
+    "Create T3 App": [
+      { text: "Introduction (Translate these!!)", link: "ar/introduction" },
+      { text: "Why CT3A?", link: "ar/why" },
+      { text: "Installation", link: "ar/installation" },
+      { text: "Folder Structure", link: "ar/folder-structure" },
+      { text: "FAQ", link: "ar/faq" },
+      { text: "T3 Collection", link: "ar/t3-collection" },
+      { text: "Other Recommendations", link: "ar/other-recs" },
+    ],
+    Usage: [
+      { text: "First Steps", link: "ar/usage/first-steps" },
+      { text: "Next.js", link: "ar/usage/next-js" },
+      { text: "TypeScript", link: "ar/usage/typescript" },
+      { text: "tRPC", link: "ar/usage/trpc" },
+      { text: "Prisma", link: "ar/usage/prisma" },
+      { text: "NextAuth.js", link: "ar/usage/next-auth" },
+      {
+        text: "Environment Variables",
+        link: "ar/usage/env-variables",
+      },
+      { text: "Tailwind CSS", link: "ar/usage/tailwind" },
+    ],
+    Deployment: [
+      { text: "Vercel", link: "ar/deployment/vercel" },
+      { text: "Docker", link: "ar/deployment/docker" },
+    ],
+  },
+  pl: {
+    "Create T3 App": [
+      { text: "Introduction (Translate these!!)", link: "pl/introduction" },
+      { text: "Why CT3A?", link: "pl/why" },
+      { text: "Installation", link: "pl/installation" },
+      { text: "Folder Structure", link: "pl/folder-structure" },
+      { text: "FAQ", link: "pl/faq" },
+      { text: "T3 Collection", link: "pl/t3-collection" },
+      { text: "Other Recommendations", link: "pl/other-recs" },
+    ],
+    Usage: [
+      { text: "First Steps", link: "pl/usage/first-steps" },
+      { text: "Next.js", link: "pl/usage/next-js" },
+      { text: "TypeScript", link: "pl/usage/typescript" },
+      { text: "tRPC", link: "pl/usage/trpc" },
+      { text: "Prisma", link: "pl/usage/prisma" },
+      { text: "NextAuth.js", link: "pl/usage/next-auth" },
+      {
+        text: "Environment Variables",
+        link: "pl/usage/env-variables",
+      },
+      { text: "Tailwind CSS", link: "pl/usage/tailwind" },
+    ],
+    Deployment: [
+      { text: "Vercel", link: "pl/deployment/vercel" },
+      { text: "Docker", link: "pl/deployment/docker" },
+    ],
+  },
+  ru: {
+    "Create T3 App": [
+      { text: "Introduction (Translate these!!)", link: "ru/introduction" },
+      { text: "Why CT3A?", link: "ru/why" },
+      { text: "Installation", link: "ru/installation" },
+      { text: "Folder Structure", link: "ru/folder-structure" },
+      { text: "FAQ", link: "ru/faq" },
+      { text: "T3 Collection", link: "ru/t3-collection" },
+      { text: "Other Recommendations", link: "ru/other-recs" },
+    ],
+    Usage: [
+      { text: "First Steps", link: "ru/usage/first-steps" },
+      { text: "Next.js", link: "ru/usage/next-js" },
+      { text: "TypeScript", link: "ru/usage/typescript" },
+      { text: "tRPC", link: "ru/usage/trpc" },
+      { text: "Prisma", link: "ru/usage/prisma" },
+      { text: "NextAuth.js", link: "ru/usage/next-auth" },
+      {
+        text: "Environment Variables",
+        link: "ru/usage/env-variables",
+      },
+      { text: "Tailwind CSS", link: "ru/usage/tailwind" },
+    ],
+    Deployment: [
+      { text: "Vercel", link: "ru/deployment/vercel" },
+      { text: "Docker", link: "ru/deployment/docker" },
     ],
   },
 };
