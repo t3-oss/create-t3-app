c4ac72c7b589723bcc8328072de8cc92946fb480
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-11-30 21:46:25 +0100

    rename NEXT_PUBLIC_BAR to NEXT_PUBLIC_CLIENTVAR

--- cli/template/addons/env/prisma-schema.mjs
+++ cli/template/addons/env/prisma-schema.mjs
@@ -16,7 +16,7 @@ export const serverSchema = z.object({
  * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 export const clientSchema = z.object({
-  // NEXT_PUBLIC_BAR: z.string(),
+  // NEXT_PUBLIC_CLIENTVAR: z.string(),
 });
 
 /**
@@ -26,5 +26,5 @@ export const clientSchema = z.object({
  * @type {{ [k in keyof z.infer<typeof clientSchema>]: z.infer<typeof clientSchema>[k] | undefined }}
  */
 export const clientEnv = {
-  // NEXT_PUBLIC_BAR: process.env.NEXT_PUBLIC_BAR,
+  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
