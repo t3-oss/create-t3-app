455b6c99c54175a4fda0a293c80780905718c95e
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-12-02 01:30:04 +0100

    feat(docs): i18n stuff (#820)
    * links in left sidebar use active language
    
    * add ToC for in progress translations
    
    * prettier
    
    * github icon rtl
    
    * implement getIsRtlFromUrl
    
    * navbar rtl fix
    
    * gap in search
    
    * use isLtr
    
    * code blocks are always ltr
    
    * leftSidebar has scrollbar on right side ltr&rtl
    
    * arabic sample files
    
    * more rtl styling
    
    * fix merge conflict
    
    * fix missing '!'
    
    * prettier
    
    * use padding-start
    
    * css cleanup
    
    * more direction-independent classes
    
    * rtl spacing stuff
    
    * fix lists and tables in rtl
    
    * fix avatar in quotes (but should be a component)
    
    * prettier
    
    * fix code embeds having rtl punctuation
    
    * slightly less awful nativeHeader type
    
    * prettier
    
    * outdated docs ltr + mx
    
    * prettier
    
    * fix ts-errors
    
    * add todo
    
    * chore: add changeset
    
    * remove changeset
    
    Co-authored-by: Julius Marminge <julius0216@outlook.com>
    Co-authored-by: Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>

--- www/src/languages.ts
+++ www/src/languages.ts
@@ -8,3 +8,27 @@ export function getLanguageFromURL(pathname: string) {
   const langCode = langCodeMatch ? langCodeMatch[1] : "en";
   return langCode as KnownLanguageCode;
 }
+
+// all RTL languages according to: https://lingohub.com/academy/best-practices/rtl-language-list
+const rtlLanguages = [
+  "ar",
+  "arc",
+  "dv",
+  "fa",
+  "ha",
+  "he",
+  "khw",
+  "ks",
+  "ku",
+  "ps",
+  "ur",
+  "yi",
+];
+
+export function getIsRtlFromUrl(pathname: string) {
+  const language = getLanguageFromURL(pathname);
+  if (rtlLanguages.indexOf(language) !== -1) {
+    return true;
+  }
+  return false;
+}
