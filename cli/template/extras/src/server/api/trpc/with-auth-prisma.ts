bd03111890d32a597c83e93f8410b654e930c238
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-12-06 23:42:22 +0100

    fix comments

--- cli/template/extras/src/server/api/trpc/with-auth-prisma.ts
+++ cli/template/extras/src/server/api/trpc/with-auth-prisma.ts
@@ -116,11 +116,11 @@ const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
 });
 
 /**
- * Public (protected) procedure
+ * Protected (authed) procedure
  *
- * This is the base piece you use to build new queries and mutations on your
- * tRPC API. It does not guarantee that a user querying is authorized, but you
- * can still access user session data if they are logged in
+ * If you want a query or mutation to ONLY be accessible to logged in users, use
+ * this. It verifies the session is valid and guarantees ctx.session.user is not
+ * null
  *
  * @see https://trpc.io/docs/procedures
  */
