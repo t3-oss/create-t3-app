76a1b86cac142930aa4af2998257f73e0a84e71a
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-12-08 21:32:10 +0100

    fix "on this page" arrow direction in rtl

--- www/src/components/navigation/OnThisPageLinks.tsx
+++ www/src/components/navigation/OnThisPageLinks.tsx
@@ -6,11 +6,13 @@ import clsx from "clsx";
 type OnThisPageLinksProps = {
   headings: MarkdownHeading[];
   title: string;
+  isRtl: boolean;
 };
 
 export default function OnThisPageLinks({
   headings,
   title,
+  isRtl,
 }: OnThisPageLinksProps) {
   const memoedHeadings = useMemo(() => {
     // add isVisible flag in headers
@@ -76,28 +78,29 @@ export default function OnThisPageLinks({
         {({ open }) => (
           <div className="relative w-full">
             <div className="">
-              <Menu.Button className="text-md inline-flex cursor-pointer items-center whitespace-nowrap rounded-md border-2 bg-t3-purple-200/50 px-3 py-2 font-medium hover:bg-t3-purple-200/75 dark:border-t3-purple-200/20 dark:bg-t3-purple-200/10 dark:hover:border-t3-purple-200/50">
+              <Menu.Button className="text-md inline-flex cursor-pointer items-center gap-1.5 whitespace-nowrap rounded-md border-2 bg-t3-purple-200/50 px-3 py-2 font-medium hover:bg-t3-purple-200/75 dark:border-t3-purple-200/20 dark:bg-t3-purple-200/10 dark:hover:border-t3-purple-200/50">
                 On this page
-                <span className="ml-1.5">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="h-3 w-3 sm:h-4 sm:w-4"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d={
-                        open
-                          ? "M19.5 8.25l-7.5 7.5-7.5-7.5"
-                          : "M8.25 4.5l7.5 7.5-7.5 7.5"
-                      }
-                    />
-                  </svg>
-                </span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className={clsx(
+                    "h-3 w-3 sm:h-4 sm:w-4",
+                    isRtl && "-scale-x-100",
+                  )}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d={
+                      open
+                        ? "M19.5 8.25l-7.5 7.5-7.5-7.5"
+                        : "M8.25 4.5l7.5 7.5-7.5 7.5"
+                    }
+                  />
+                </svg>
               </Menu.Button>
             </div>
             <Menu.Items
