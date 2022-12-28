a903477494de3b454944a3c9b211a34dae99e763
Nick Schmidt <nick@fintory.com>
2022-12-28 11:32:44 +0100

    style: run prettier to format files

--- cli/template/base/src/env/schema.mjs
+++ cli/template/base/src/env/schema.mjs
@@ -10,8 +10,8 @@ export const serverSchema = z.object({
 });
 
 /**
- * You can't destruct `process.env` as a regular object in the Next.js 
- * middleware, so you have to do it manually here. 
+ * You can't destruct `process.env` as a regular object in the Next.js
+ * middleware, so you have to do it manually here.
  * @type {{ [k in keyof z.infer<typeof serverSchema>]: z.infer<typeof serverSchema>[k] | undefined }}
  */
 export const serverEnv = {
