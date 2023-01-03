b04776945d317366b726117bd98dd04a431a0ff4
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-01-03 12:52:00 +0100

    re-enable some disabled overrides

--- cli/template/extras/src/pages/index/with-auth-trpc-tw.tsx
+++ cli/template/extras/src/pages/index/with-auth-trpc-tw.tsx
@@ -74,7 +74,7 @@ const AuthShowcase: React.FC = () => {
       </p>
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => signOut() : () => signIn()}
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
       >
         {sessionData ? "Sign out" : "Sign in"}
       </button>
