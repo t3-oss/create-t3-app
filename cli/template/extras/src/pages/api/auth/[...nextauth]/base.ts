cfcb475731e4a9df4442009b0358259e5899f792
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2023-01-03 19:27:33 +0900

    add note about other providers (#1046)

--- cli/template/extras/src/pages/api/auth/[...nextauth]/base.ts
+++ cli/template/extras/src/pages/api/auth/[...nextauth]/base.ts
@@ -19,7 +19,15 @@ export const authOptions: NextAuthOptions = {
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
-    // ...add more providers here
+    /**
+     * ...add more providers here
+     *
+     * Most other providers require a bit more work than the Discord provider.
+     * For example, the GitHub provider requires you to add the
+     * `refresh_token_expires_in` field to the Account model. Refer to the
+     * NextAuth.js docs for the provider you want to use. Example:
+     * @see https://next-auth.js.org/providers/github
+     */
   ],
 };
 
