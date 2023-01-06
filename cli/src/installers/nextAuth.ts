96ea8c2e2351ce38c7fb2e751c402fb6acaf54f9
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-01-07 00:52:46 +0100

    fix bad import

--- cli/src/installers/nextAuth.ts
+++ cli/src/installers/nextAuth.ts
@@ -1,5 +1,5 @@
 import type { Installer } from "~/installers/index.js";
-import { AvailableDependencies } from "~/installers/dependencyVersionMap";
+import { AvailableDependencies } from "~/installers/dependencyVersionMap.js";
 import path from "path";
 import fs from "fs-extra";
 import { PKG_ROOT } from "~/consts.js";
