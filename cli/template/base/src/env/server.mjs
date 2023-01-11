cdcc203c2d25ec24a60c80b78597cc6bf072ce51
Jacob Devore <jacob@blient.com>
2023-01-11 14:26:10 -0500

    Deprecate Manual Declarations of serverEnv

--- cli/template/base/src/env/server.mjs
+++ cli/template/base/src/env/server.mjs
@@ -3,8 +3,20 @@
  * This file is included in `/next.config.mjs` which ensures the app isn't built with invalid env vars.
  * It has to be a `.mjs`-file to be imported there.
  */
-import { serverSchema, serverEnv } from "./schema.mjs";
+import { serverSchema } from "./schema.mjs";
 import { env as clientEnv, formatErrors } from "./client.mjs";
+
+/** @type {{ [key: string]: string | undefined; }} */
+let serverEnv = {};
+
+/**
+ * You can't destruct `process.env` as a regular object, so you have to do
+ * it manually here. This is because Next.js evaluates this at build time,
+ * and only used environment variables are included in the build.
+ */
+Object.keys(serverSchema.shape).forEach(
+  (key) => (serverEnv[key] = process.env[key]),
+);
 
 const _serverEnv = serverSchema.safeParse(serverEnv);
 
