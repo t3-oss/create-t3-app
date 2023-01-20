3eabd71cb6bedbd7861d7351098ea93a2ab2421c
xoldyckk <xoldyckk@gmail.com>
2023-01-20 23:17:50 +0530

    changed VERCEL to VERCEL_URL in serverSchema

--- cli/template/extras/src/env/schema/with-auth-prisma.mjs
+++ cli/template/extras/src/env/schema/with-auth-prisma.mjs
@@ -17,7 +17,7 @@ export const serverSchema = z.object({
     // Since NextAuth.js automatically uses the VERCEL_URL if present.
     (str) => process.env.VERCEL_URL ?? str,
     // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-    process.env.VERCEL ? z.string() : z.string().url(),
+    process.env.VERCEL_URL ? z.string() : z.string().url(),
   ),
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
