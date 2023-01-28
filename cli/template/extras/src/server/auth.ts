94925760a1cc3e32115bf128dfc3b976af1f3059
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-01-28 13:57:22 +0100

    chore: bump dep and remove unstable

--- cli/template/extras/src/server/auth.ts
+++ cli/template/extras/src/server/auth.ts
@@ -1,15 +1,11 @@
 import { type GetServerSidePropsContext } from "next";
-import { unstable_getServerSession } from "next-auth";
+import { getServerSession } from "next-auth";
 
 import { authOptions } from "../pages/api/auth/[...nextauth]";
 
 /**
- * Wrapper for unstable_getServerSession, used in trpc createContext and the
- * restricted API route
- *
- * Don't worry too much about the "unstable", it's safe to use but the syntax
- * may change in future versions
- *
+ * Wrapper for getServerSession so that you don't need
+ * to import the authOptions in every file.
  * @see https://next-auth.js.org/configuration/nextjs
  */
 
@@ -17,5 +13,5 @@ export const getServerAuthSession = async (ctx: {
   req: GetServerSidePropsContext["req"];
   res: GetServerSidePropsContext["res"];
 }) => {
-  return await unstable_getServerSession(ctx.req, ctx.res, authOptions);
+  return await getServerSession(ctx.req, ctx.res, authOptions);
 };
