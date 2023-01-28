b773c8dd679f6ffeb02669466328e49a6128dcd2
Buck, Sebastian <sebastian.buck@adesso.de>
2023-01-28 14:21:26 +0100

    fix set: string or undefined for environment

--- cli/template/extras/src/env/schema/with-auth.mjs
+++ cli/template/extras/src/env/schema/with-auth.mjs
@@ -25,7 +25,7 @@ export const serverSchema = z.object({
 /**
  * You can't destruct `process.env` as a regular object in the Next.js
  * middleware, so you have to do it manually here.
- * @type {{ [k in keyof z.input<typeof serverSchema>]: z.input<typeof serverSchema>[k] | undefined }}
+ * @type {{ [k in keyof z.input<typeof serverSchema>]: string | undefined }}
  */
 export const serverEnv = {
   NODE_ENV: process.env.NODE_ENV,
@@ -48,7 +48,7 @@ export const clientSchema = z.object({
  * You can't destruct `process.env` as a regular object, so you have to do
  * it manually here. This is because Next.js evaluates this at build time,
  * and only used environment variables are included in the build.
- * @type {{ [k in keyof z.input<typeof clientSchema>]: z.input<typeof clientSchema>[k] | undefined }}
+ * @type {{ [k in keyof z.input<typeof clientSchema>]: string | undefined }}
  */
 export const clientEnv = {
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
