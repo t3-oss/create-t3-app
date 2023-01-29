e247702af98d112d3d26b8ff1dcc8f19e0aa8ab2
James <5511220+Zamiell@users.noreply.github.com>
2023-01-29 05:05:55 -0500

    fix: env vars (#1140)

---
+++ .changeset/rich-seas-speak.md
@@ -0,0 +1,5 @@
+---
+"create-t3-app": patch
+---
+
+chore: add descriptive comments and format .env prettier
--- cli/src/installers/envVars.ts
+++ cli/src/installers/envVars.ts
@@ -36,42 +36,52 @@ export const envVariablesInstaller: Installer = ({ projectDir, packages }) => {
 };
 
 const getEnvContent = (usingAuth: boolean, usingPrisma: boolean) => {
-  let content =
-    "# When adding additional env variables, the schema in /env/schema.mjs should be updated accordingly";
+  let content = `
+# When adding additional environment variables, the schema in "/env/schema.mjs"
+# should be updated accordingly.
+`
+    .trim()
+    .concat("\n");
 
   if (usingPrisma)
     content += `
 # Prisma
-DATABASE_URL=file:./db.sqlite
+# https://www.prisma.io/docs/reference/database-reference/connection-urls#env
+DATABASE_URL="file:./db.sqlite"
 `;
 
   if (usingAuth)
     content += `
 # Next Auth
-# You can generate the secret via 'openssl rand -base64 32' on Linux
-# More info: https://next-auth.js.org/configuration/options#secret
-# NEXTAUTH_SECRET=
-NEXTAUTH_URL=http://localhost:3000
+# You can generate a new secret on the command line with:
+# openssl rand -base64 32
+# https://next-auth.js.org/configuration/options#secret
+#NEXTAUTH_SECRET=""
+NEXTAUTH_URL="http://localhost:3000"
 
 # Next Auth Discord Provider
-DISCORD_CLIENT_ID=
-DISCORD_CLIENT_SECRET=
+DISCORD_CLIENT_ID=""
+DISCORD_CLIENT_SECRET=""
 `;
 
   if (!usingAuth && !usingPrisma)
     content += `
 # Example:
-# SERVERVAR=foo
-# NEXT_PUBLIC_CLIENTVAR=bar
+# SERVERVAR="foo"
+# NEXT_PUBLIC_CLIENTVAR="bar"
 `;
 
   return content;
 };
 
-const exampleEnvContent = `# Since .env is gitignored, you can use .env.example to build a new \`.env\` file when you clone the repo.
-# Keep this file up-to-date when you add new variables to \`.env\`.
+const exampleEnvContent = `
+# Since the ".env" file is gitignored, you can use the ".env.example" file to
+# build a new ".env" file when you clone the repo. Keep this file up-to-date
+# when you add new variables to \`.env\`.
 
-# This file will be committed to version control, so make sure not to have any secrets in it.
-# If you are cloning this repo, create a copy of this file named \`.env\` and populate it with your secrets.
-
-`;
+# This file will be committed to version control, so make sure not to have any
+# secrets in it. If you are cloning this repo, create a copy of this file named
+# ".env" and populate it with your secrets.
+`
+  .trim()
+  .concat("\n\n");
