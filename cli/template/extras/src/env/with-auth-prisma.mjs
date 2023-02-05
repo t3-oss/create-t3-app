054d1431531afb617a6e4537acafa0d47af801e0
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2023-02-05 22:25:58 +0100

    dont crash when calling next-auth in gssp

--- cli/template/extras/src/env/with-auth-prisma.mjs
+++ cli/template/extras/src/env/with-auth-prisma.mjs
@@ -50,45 +50,57 @@ const processEnv = {
 // Don't touch the part below
 // --------------------------
 
-const formatErrors = (
-  /** @type {z.ZodFormattedError<Map<string,string>,string>} */
-  errors,
-) =>
-  Object.entries(errors)
-    .map(([name, value]) => {
-      if (value && "_errors" in value)
-        return `${name}: ${value._errors.join(", ")}\n`;
-    })
-    .filter(Boolean);
-
-const isServer = typeof window === "undefined";
-
 const merged = server.merge(client);
-const parsed = isServer
-  ? merged.safeParse(processEnv) // on server we can validate all env vars
-  : client.safeParse(processEnv); // on client we can only validate the ones that are exposed
-
-if (parsed.success === false) {
-  console.error(
-    "❌ Invalid environment variables:\n",
-    ...formatErrors(parsed.error.format()),
-  );
-  throw new Error("Invalid environment variables");
-}
-
 /** @type z.infer<merged>
  *  @ts-ignore - can't type this properly in jsdoc */
-export const env = new Proxy(parsed.data, {
-  get(target, prop) {
-    if (typeof prop !== "string") return undefined;
-    // Throw a descriptive error if a server-side env var is accessed on the client
-    // Otherwise it would just be returning `undefined` and be annoying to debug
-    if (!isServer && !prop.startsWith("NEXT_PUBLIC_"))
-      throw new Error(
-        `❌ Attempted to access server-side environment variable '${prop}' on the client`,
-      );
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore - can't type this properly in jsdoc
-    return target[prop];
-  },
-});
+let env;
+
+if (!process.env.SKIP_ENV_VALIDATION) {
+  const formatErrors = (
+    /** @type {z.ZodFormattedError<Map<string,string>,string>} */
+    errors,
+  ) =>
+    Object.entries(errors)
+      .map(([name, value]) => {
+        if (value && "_errors" in value)
+          return `${name}: ${value._errors.join(", ")}\n`;
+      })
+      .filter(Boolean);
+
+  const isServer = typeof window === "undefined";
+
+  const parsed = isServer
+    ? merged.safeParse(processEnv) // on server we can validate all env vars
+    : client.safeParse(processEnv); // on client we can only validate the ones that are exposed
+
+  if (parsed.success === false) {
+    console.error(
+      "❌ Invalid environment variables:\n",
+      ...formatErrors(parsed.error.format()),
+    );
+    throw new Error("Invalid environment variables");
+  }
+
+  /** @type z.infer<merged>
+   *  @ts-ignore - can't type this properly in jsdoc */
+  env = new Proxy(parsed.data, {
+    get(target, prop) {
+      if (typeof prop !== "string") return undefined;
+      // Throw a descriptive error if a server-side env var is accessed on the client
+      // Otherwise it would just be returning `undefined` and be annoying to debug
+      if (!isServer && !prop.startsWith("NEXT_PUBLIC_"))
+        throw new Error(
+          `❌ Attempted to access server-side environment variable '${prop}' on the client`,
+        );
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore - can't type this properly in jsdoc
+      return target[prop];
+    },
+  });
+} else {
+  /** @type z.infer<merged>
+   *  @ts-ignore - can't type this properly in jsdoc */
+  env = processEnv;
+}
+
+export { env };
