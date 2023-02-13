3ccbeb292e0ab2e7dd285819e5f8b22304699506
Julius Marminge <julius0216@outlook.com>
2023-02-13 08:31:28 +0100

    prettier + explicit error + comment

--- www/.eslintrc.cjs
+++ www/.eslintrc.cjs
@@ -39,7 +39,7 @@ module.exports = {
         "no-unused-vars": [
           "error",
           { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
-        ],       
+        ],
       },
     },
   ],
