9ce7f3121eb0094e8aca49808f86e744df632953
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-02-19 09:41:38 +0100

    cleanup more

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,4 @@
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
   plugins: [
@@ -10,36 +11,11 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
   ],
-  overrides: [
-    {
-      extends: [
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      ],
-      files: ["*.ts", "*.tsx"],
-      parserOptions: {
-        project: "tsconfig.json",
-      },
-      rules: {
-        "@typescript-eslint/require-await": "off",
-        "@typescript-eslint/restrict-plus-operands": "off",
-        "@typescript-eslint/restrict-template-expressions": "off",
-
-        // These rules are only disabled because we hit a bug in linting.
-        // See https://github.com/t3-oss/create-t3-app/pull/1036#discussion_r1060505136
-        // If you still see the bug once TypeScript@5 is used, please let typescript-eslint know!
-        "@typescript-eslint/no-unsafe-argument": "off",
-        "@typescript-eslint/no-unsafe-assignment": "off",
-        "@typescript-eslint/no-unsafe-call": "off",
-        "@typescript-eslint/no-unsafe-call": "off",
-        "@typescript-eslint/no-unsafe-member-access": "off",
-        "@typescript-eslint/no-unsafe-return": "off",
-      },
-    },
-  ],
   parserOptions: {
-    ecmaVersion: "latest", // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of import
-    project: "./tsconfig.eslint.json", // Allows for the use of rules which require parserServices to be generated
+    ecmaVersion: "latest",
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json", "./cli/tsconfig.json", "./www/tsconfig.json"],
   },
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs.
@@ -63,4 +39,37 @@ module.exports = {
     "isaacscript/complete-sentences-jsdoc": "warn",
     "isaacscript/format-jsdoc-comments": "warn",
   },
+  overrides: [
+    {
+      extends: [
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      files: ["**/*.ts", "**/*.tsx"],
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        tsconfigRootDir: __dirname,
+        project: [
+          "./tsconfig.json",
+          "./cli/tsconfig.json",
+          "./www/tsconfig.json",
+        ],
+      },
+      rules: {
+        "@typescript-eslint/require-await": "off",
+        "@typescript-eslint/restrict-plus-operands": "off",
+        "@typescript-eslint/restrict-template-expressions": "off",
+
+        // These rules are only disabled because we hit a bug in linting.
+        // See https://github.com/t3-oss/create-t3-app/pull/1036#discussion_r1060505136
+        // If you still see the bug once TypeScript@5 is used, please let typescript-eslint know!
+        "@typescript-eslint/no-unsafe-argument": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-return": "off",
+      },
+    },
+  ],
 };
--- cli/template/extras/src/server/api/root.ts
+++ cli/template/extras/src/server/api/root.ts
@@ -4,7 +4,7 @@ import { exampleRouter } from "~/server/api/routers/example";
 /**
  * This is the primary router for your server.
  *
- * All routers added in /api/routers should be manually added here
+ * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
