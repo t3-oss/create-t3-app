c0b1f3b3f1d77585c8144d948bbbc231591161cf
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-02-19 11:05:06 +0100

    remove prettier rule

--- www/.eslintrc.cjs
+++ www/.eslintrc.cjs
@@ -12,7 +12,6 @@ module.exports = {
         extraFileExtensions: [".astro"],
       },
       rules: {
-        "prettier/prettier": "off",
         "no-unused-vars": [
           "error",
           { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
