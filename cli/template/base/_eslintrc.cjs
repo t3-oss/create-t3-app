224b56f2514c090eefd841a2dc590afed8c86693
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-02-19 12:06:00 +0100

    restore

--- cli/template/base/_eslintrc.cjs
+++ cli/template/base/_eslintrc.cjs
@@ -18,6 +18,12 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
-    "@typescript-eslint/consistent-type-imports": "warn",
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
   },
 };
