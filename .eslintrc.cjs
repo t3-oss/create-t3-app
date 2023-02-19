457375cce05209811f2b67fccac2b45f5831f2df
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-02-19 11:20:31 +0100

    cleanup eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,11 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
-  plugins: [
-    // Provides extra useful rules:
-    // https://github.com/IsaacScript/isaacscript/tree/main/packages/eslint-plugin-isaacscript
-    "isaacscript",
-    "import",
-  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["isaacscript", "import"],
   extends: [
     "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:prettier/recommended",
   ],
   parserOptions: {
@@ -23,7 +19,6 @@ module.exports = {
     ],
   },
   rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs.
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/no-unused-vars": [
@@ -38,42 +33,22 @@ module.exports = {
       },
     ],
     "import/consistent-type-specifier-style": ["error", "prefer-inline"],
-    // These rules auto-format comments, similar to how Prettier auto-formats code. For more
-    // information, see:
-    // https://github.com/IsaacScript/isaacscript/blob/main/packages/eslint-plugin-isaacscript/docs/comments.md
+
+    "@typescript-eslint/require-await": "off",
+    "@typescript-eslint/restrict-plus-operands": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+
+    // These rules are only disabled because we hit a bug in linting.
+    // See https://github.com/t3-oss/create-t3-app/pull/1036#discussion_r1060505136
+    // If you still see the bug once TypeScript@5 is used, please let typescript-eslint know!
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+
+    // For educational purposes we format our comments/jsdoc nicely
     "isaacscript/complete-sentences-jsdoc": "warn",
     "isaacscript/format-jsdoc-comments": "warn",
   },
-  overrides: [
-    {
-      extends: [
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      ],
-      files: ["**/*.ts", "**/*.tsx"],
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        tsconfigRootDir: __dirname,
-        project: [
-          "./tsconfig.json",
-          "./cli/tsconfig.json",
-          "./www/tsconfig.json",
-        ],
-      },
-      rules: {
-        "@typescript-eslint/require-await": "off",
-        "@typescript-eslint/restrict-plus-operands": "off",
-        "@typescript-eslint/restrict-template-expressions": "off",
-
-        // These rules are only disabled because we hit a bug in linting.
-        // See https://github.com/t3-oss/create-t3-app/pull/1036#discussion_r1060505136
-        // If you still see the bug once TypeScript@5 is used, please let typescript-eslint know!
-        "@typescript-eslint/no-unsafe-argument": "off",
-        "@typescript-eslint/no-unsafe-assignment": "off",
-        "@typescript-eslint/no-unsafe-call": "off",
-        "@typescript-eslint/no-unsafe-member-access": "off",
-        "@typescript-eslint/no-unsafe-return": "off",
-      },
-    },
-  ],
 };
--- www/.eslintrc.cjs
+++ www/.eslintrc.cjs
@@ -3,30 +3,4 @@ module.exports = {
   parserOptions: {
     extraFileExtensions: [".astro"],
   },
-  overrides: [
-    {
-      files: ["*.astro"],
-      parser: "astro-eslint-parser",
-      parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
-      },
-      rules: {
-        "no-unused-vars": [
-          "error",
-          { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
-        ],
-      },
-    },
-    {
-      files: ["*.ts", "*.tsx"],
-      parser: "@typescript-eslint/parser",
-      rules: {
-        "no-unused-vars": [
-          "error",
-          { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
-        ],
-      },
-    },
-  ],
 };
