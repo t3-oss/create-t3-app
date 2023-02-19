73bd80eb26e5efd78a9fdc097bdbff2849a3d75d
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2023-02-20 00:13:24 +0100

    new selectBoilerplate

--- cli/src/helpers/selectBoilerplate.ts
+++ cli/src/helpers/selectBoilerplate.ts
@@ -39,24 +39,14 @@ export const selectIndexFile = ({
 }: SelectBoilerplateProps) => {
   const indexFileDir = path.join(PKG_ROOT, "template/extras/src/pages/index");
 
-  const usingTRPC = packages.trpc.inUse;
-  const usingTw = packages.tailwind.inUse;
-  const usingAuth = packages.nextAuth.inUse;
+  let packageList = "";
+  if (packages.nextAuth.inUse) packageList += "-auth";
+  if (packages.prisma.inUse) packageList += "-prisma";
+  if (packages.trpc.inUse) packageList += "-trpc";
+  if (packages.tailwind.inUse) packageList += "-tw";
 
-  let indexFile = "";
-  if (usingTRPC && usingTw && usingAuth) {
-    indexFile = "with-auth-trpc-tw.tsx";
-  } else if (usingTRPC && !usingTw && usingAuth) {
-    indexFile = "with-auth-trpc.tsx";
-  } else if (usingTRPC && usingTw) {
-    indexFile = "with-trpc-tw.tsx";
-  } else if (usingTRPC && !usingTw) {
-    indexFile = "with-trpc.tsx";
-  } else if (!usingTRPC && usingTw) {
-    indexFile = "with-tw.tsx";
-  }
-
-  if (indexFile !== "") {
+  if (packageList !== "") {
+    const indexFile = `with${packageList}.tsx`;
     const indexSrc = path.join(indexFileDir, indexFile);
     const indexDest = path.join(projectDir, "src/pages/index.tsx");
     fs.copySync(indexSrc, indexDest);
