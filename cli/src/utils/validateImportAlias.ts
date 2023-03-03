230bd016b58d23a39a9810aea90795c7a4643532
Justas Samuolis <samuolis.just@gmail.com>
2023-03-03 17:38:37 +0200

    feat: Add validation for import aliases (#1255)
    * feat: do not allow invalid import aliases
    
    * chore: add changeset
    
    * fix: use validate instead of custom logic
    
    * fix: update changeset to better match changes
    
    * refactor: change naming from path alias to import alias
    
    ---------
    
    Co-authored-by: Christopher Ehrlich <ehrlich.christopher@gmail.com>

---
+++ cli/src/utils/validateImportAlias.ts
@@ -0,0 +1,7 @@
+export const validateImportAlias = (input: string) => {
+  if (input.startsWith(".") || input.startsWith("/")) {
+    return "Import alias can't start with '.' or '/'";
+  } else {
+    return true;
+  }
+};
