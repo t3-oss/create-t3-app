10a5e0b2fe91b887526960515c6422791a77c724
Julius Marminge <julius0216@outlook.com>
2023-03-16 14:15:25 +0100

    feat: add errorformatter (#1273)
    * add errorformatter
    
    * add docs
    
    * changeset
    
    * Update www/src/pages/en/usage/trpc.md
    
    Co-authored-by: Christopher Ehrlich <ehrlich.christopher@gmail.com>
    
    ---------
    
    Co-authored-by: Christopher Ehrlich <ehrlich.christopher@gmail.com>
    Co-authored-by: Shoubhit Dash <shoubhit2005@gmail.com>

--- cli/template/extras/src/server/api/trpc/with-auth-prisma.ts
+++ cli/template/extras/src/server/api/trpc/with-auth-prisma.ts
@@ -61,15 +61,25 @@ export const createTRPCContext = async (opts: CreateNextContextOptions) => {
 /**
  * 2. INITIALIZATION
  *
- * This is where the tRPC API is initialized, connecting the context and transformer.
+ * This is where the tRPC API is initialized, connecting the context and transformer. We also parse
+ * ZodErrors so that you get typesafety on the frontend if your procedure fails due to validation
+ * errors on the backend.
  */
 import { initTRPC, TRPCError } from "@trpc/server";
 import superjson from "superjson";
+import { ZodError } from "zod";
 
 const t = initTRPC.context<typeof createTRPCContext>().create({
   transformer: superjson,
-  errorFormatter({ shape }) {
-    return shape;
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.cause instanceof ZodError ? error.cause.flatten() : null,
+      },
+    };
   },
 });
 
