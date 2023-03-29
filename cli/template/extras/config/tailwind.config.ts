526f2fd422cda209b7b8e9dec7ed517c12e7f30f
Finn <34718806+FinnDore@users.noreply.github.com>
2023-03-29 17:28:37 +0100

    updated the cli template

--- cli/template/extras/config/tailwind.config.ts
+++ cli/template/extras/config/tailwind.config.ts
@@ -1,10 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-const config = {
+import { type Config } from "tailwindcss";
+
+export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
-
-module.exports = config;
+} satisfies Config;
