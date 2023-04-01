7fcb41c95d24567040b72409a576d8c694f910a1
Jing Li <jing.li.1998.jl@gmail.com>
2023-04-02 06:58:58 +1000

    fix: update types for global prisma instance (#1321)
    * fix: update types for global prisma instance
    
    * chore: add changeset
    
    ---------
    
    Co-authored-by: Jing Li <jli4@atlassian.com>

--- cli/template/extras/src/server/db.ts
+++ cli/template/extras/src/server/db.ts
@@ -2,10 +2,12 @@ import { PrismaClient } from "@prisma/client";
 
 import { env } from "~/env.mjs";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
 export const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
