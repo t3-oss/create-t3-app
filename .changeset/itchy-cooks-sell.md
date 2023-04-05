d6618cf507baa9810830637315f2d0744b221cea
Julius Marminge <julius0216@outlook.com>
2023-04-06 00:29:05 +0200

    fix: create a new object for the session callback (#1330)
    * spread new object
    
    * whoops
    
    * Create .changeset/itchy-cooks-sell.md
    
    ---------
    
    Co-authored-by: Christopher Ehrlich <ehrlich.christopher@gmail.com>

---
+++ .changeset/itchy-cooks-sell.md
@@ -0,0 +1,5 @@
+---
+"create-t3-app": patch
+---
+
+fix: create a new object for the session callback
--- cli/template/extras/src/server/auth/base.ts
+++ cli/template/extras/src/server/auth/base.ts
@@ -35,13 +35,13 @@ declare module "next-auth" {
  */
 export const authOptions: NextAuthOptions = {
   callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-        // session.user.role = user.role; <-- put other properties on the session here
-      }
-      return session;
-    },
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+      },
+    }),
   },
   providers: [
     DiscordProvider({
