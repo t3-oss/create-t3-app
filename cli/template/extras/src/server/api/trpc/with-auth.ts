c48b38f522b81097e2f0e883ede053dbac620c12
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2023-04-11 17:06:10 +0200

    refactor: add destructuring (#1352)
    * refactor: code by removing unnecessary destructuring and adding session destructuring.
    
    * refactor: code by removing unnecessary destructuring and adding session destructuring.
    
    * Lint
    
    * Delete sour-laws-serve.md
    
    * Create .changeset/wet-flies-train.md
    
    ---------
    
    Co-authored-by: Julius Marminge <julius0216@outlook.com>

--- cli/template/extras/src/server/api/trpc/with-auth.ts
+++ cli/template/extras/src/server/api/trpc/with-auth.ts
@@ -33,9 +33,9 @@ type CreateContextOptions = {
  *
  * @see https://create.t3.gg/en/usage/trpc#-serverapitrpcts
  */
-const createInnerTRPCContext = (opts: CreateContextOptions) => {
+const createInnerTRPCContext = ({ session }: CreateContextOptions) => {
   return {
-    session: opts.session,
+    session,
   };
 };
 
@@ -45,9 +45,10 @@ const createInnerTRPCContext = (opts: CreateContextOptions) => {
  *
  * @see https://trpc.io/docs/context
  */
-export const createTRPCContext = async (opts: CreateNextContextOptions) => {
-  const { req, res } = opts;
-
+export const createTRPCContext = async ({
+  req,
+  res,
+}: CreateNextContextOptions) => {
   // Get the session from the server using the getServerSession wrapper function
   const session = await getServerAuthSession({ req, res });
 
