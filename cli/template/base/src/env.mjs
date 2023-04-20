3e02e28c0e76912ce1e1a2a4958af07ef035e7c9
Chung Wei <leongchungwei@hotmail.com>
2023-04-20 23:44:04 +0800

    fix: Revert code

--- cli/template/base/src/env.mjs
+++ cli/template/base/src/env.mjs
@@ -13,9 +13,11 @@ const server = z.object({
  * built with invalid env vars. To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 const client = z.object(
-  /** @satisfies {Record<`NEXT_PUBLIC_${string}`, import('zod').ZodType>} */ ({
-    NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
-  }),
+  /** @satisfies {Record<`NEXT_PUBLIC_${string}`, import('zod').ZodType>} */ (
+    {
+      // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
+    }
+  ),
 );
 
 /**
