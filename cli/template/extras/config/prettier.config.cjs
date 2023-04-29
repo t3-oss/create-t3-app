d8169926eb5e78720cfdb2e250fb4c2da5e75bd4
Finn <34718806+FinnDore@users.noreply.github.com>
2023-04-29 13:50:46 +0100

    cli format

--- cli/template/extras/config/prettier.config.cjs
+++ cli/template/extras/config/prettier.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  plugins: [
+    require.resolve("prettier-plugin-tailwindcss"),
+    require.resolve("@ianvs/prettier-plugin-sort-imports"),
+  ],
 };
 
 module.exports = config;
