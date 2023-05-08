e29087f8367701d44b0d4c8ef9e0a626201303cd
Finn <34718806+FinnDore@users.noreply.github.com>
2023-05-08 19:20:56 +0100

    format cli

--- cli/.prettierrc.cjs
+++ cli/.prettierrc.cjs
@@ -1,10 +1,12 @@
-
 const baseConfig = require("../.prettierrc.cjs");
 
 /** @type {import('prettier').Config} */
 const config = {
   ...baseConfig,
-  plugins: [...baseConfig.plugins, require.resolve("prettier-plugin-tailwindcss")],
+  plugins: [
+    ...baseConfig.plugins,
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
   tailwindConfig: "./template/extras/config/tailwind.config.ts",
 };
 
--- cli/template/extras/src/env/with-auth-prisma.mjs
+++ cli/template/extras/src/env/with-auth-prisma.mjs
@@ -1,5 +1,5 @@
-import { z } from "zod";
 import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
 
 export const env = createEnv({
   /**
