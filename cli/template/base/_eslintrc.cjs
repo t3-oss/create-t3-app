86d07ed024b8a63c555f750005cf1f726bcd0b2f
Finn <34718806+FinnDore@users.noreply.github.com>
2023-05-09 19:44:19 +0100

    fixed base project

--- cli/template/base/_eslintrc.cjs
+++ cli/template/base/_eslintrc.cjs
@@ -1,4 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
 /** @type {import("eslint").Linter.Config} */
-const config = {};
+const config = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: path.join(__dirname, "tsconfig.json"),
+  },
+  plugins: ["@typescript-eslint"],
+};
 
 module.exports = config;
