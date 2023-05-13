db03947acff059caca9fc96133d17600baec6f8e
Finn <34718806+FinnDore@users.noreply.github.com>
2023-05-13 21:45:32 +0100

    removed uneeded resolve

--- cli/template/extras/config/prettier-with-tailwind.config.cjs
+++ cli/template/extras/config/prettier-with-tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
 };
 
 module.exports = config;
