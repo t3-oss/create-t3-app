a7b179253883e0180be2b126ac649875674b5fe4
juliusmarminge <julius0216@outlook.com>
2023-05-15 19:54:06 +0200

    initial workspace config

--- upgrade/next.config.mjs
+++ upgrade/next.config.mjs
@@ -7,16 +7,5 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
 };
 export default config;
--- upgrade/prettier.config.cjs
+++ upgrade/prettier.config.cjs
@@ -1,6 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const baseConfig = require("../.prettierrc.cjs");
+
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  ...baseConfig,
+  plugins: [
+    ...(baseConfig.plugins ?? []),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
 };
 
 module.exports = config;
