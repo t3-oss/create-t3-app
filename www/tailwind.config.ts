fa5f6fab366f81f996056bddf62ad6e41cb2035c
Caglar <61601796+violetbee@users.noreply.github.com>
2023-07-04 18:41:22 +0300

    Update tailwind.config.ts
    export default does not work properly, converted to module exports

--- www/tailwind.config.ts
+++ www/tailwind.config.ts
@@ -1,6 +1,6 @@
-import { type Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
 
-export default {
+module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
