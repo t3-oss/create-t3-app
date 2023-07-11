ecc8ab26fa683b5a814e832f2061a724c781dffe
Levin Uncu <levin@uncu.de>
2023-07-11 22:41:15 +0200

    feat(cli): Show Next Git steps based on current git repo

--- cli/src/helpers/logNextSteps.ts
+++ cli/src/helpers/logNextSteps.ts
@@ -1,14 +1,19 @@
+import { isInsideGitRepo, isRootGitRepo } from "./git.js";
 import { DEFAULT_APP_NAME } from "~/consts.js";
 import { type InstallerOptions } from "~/installers/index.js";
 import { getUserPkgManager } from "~/utils/getUserPkgManager.js";
 import { logger } from "~/utils/logger.js";
 
 // This logs the next steps that the user should take in order to advance the project
-export const logNextSteps = ({
+export const logNextSteps = async ({
   projectName = DEFAULT_APP_NAME,
   packages,
   noInstall,
-}: Pick<InstallerOptions, "projectName" | "packages" | "noInstall">) => {
+  projectDir,
+}: Pick<
+  InstallerOptions,
+  "projectName" | "packages" | "noInstall" | "projectDir"
+>) => {
   const pkgManager = getUserPkgManager();
 
   logger.info("Next steps:");
@@ -30,5 +35,9 @@ export const logNextSteps = ({
 
   logger.info(`  ${pkgManager === "npm" ? "npm run" : pkgManager} dev`);
 
-  logger.info(`  git commit -m "initial commit"`);
+  if ((await isInsideGitRepo(projectDir)) || isRootGitRepo(projectDir)) {
+    logger.info(`  git commit -m "initial commit"`);
+  } else {
+    logger.info(`  git init`);
+  }
 };
