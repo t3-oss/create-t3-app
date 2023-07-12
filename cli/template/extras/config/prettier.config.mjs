dc0878beea11490abc4df65c4a425e95cd2e6be2
Finn <34718806+FinnDore@users.noreply.github.com>
2023-07-12 18:43:18 +0100

    template

---
+++ cli/template/extras/config/prettier.config.mjs
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import prettierPluginTailwindcss from "prettier-plugin-tailwindcss";
+
+/** @type {import("prettier").Config} */
+export default {
+  plugins: [prettierPluginTailwindcss],
+};
--- cli/template/extras/config/prettier.config.ts
+++
@@ -1,5 +0,0 @@
-import { type Config } from "prettier";
-
-export default {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
-} satisfies Config;
