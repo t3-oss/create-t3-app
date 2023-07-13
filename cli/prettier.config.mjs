09f2846a875c678c0cf57958f07fea01bc1e2340
Finn <34718806+FinnDore@users.noreply.github.com>
2023-07-13 19:19:39 +0100

    cli formats it anyways

--- cli/prettier.config.mjs
+++ cli/prettier.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('prettier').Config} */
 const config = {
   ...baseConfig,
-  arrowParens: 'alway',
-  plugins: [...baseConfig.plugins, 'prettier-plugin-tailwindcss'],
-  tailwindConfig: './template/extras/config/tailwind.config.ts',
-  trailingComma: 'es5',
+  arrowParens: "alway",
+  plugins: [...baseConfig.plugins, "prettier-plugin-tailwindcss"],
+  tailwindConfig: "./template/extras/config/tailwind.config.ts",
+  trailingComma: "es5",
 };
 
 export default config;
--- cli/template/extras/config/prettier.config.mjs
+++ cli/template/extras/config/prettier.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: ['prettier-plugin-tailwindcss'],
+  plugins: ["prettier-plugin-tailwindcss"],
 };
 
 export default config;
