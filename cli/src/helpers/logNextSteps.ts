5b4d054fc35ceed31ab50424ec5e219bd2bcc30d
juliusmarminge <julius0216@outlook.com>
2023-08-10 23:57:37 +0200

    fixx

--- cli/src/helpers/logNextSteps.ts
+++ cli/src/helpers/logNextSteps.ts
@@ -27,8 +27,7 @@ export const logNextSteps = async ({
     }
   }
 
-  const drizzleInUse = packages?.drizzle;
-  if (packages?.prisma.inUse || drizzleInUse) {
+  if (packages?.prisma.inUse || packages?.drizzle.inUse) {
     logger.info(`  ${pkgManager === "npm" ? "npx" : pkgManager} db:push`);
   }
 
@@ -39,7 +38,7 @@ export const logNextSteps = async ({
   }
   logger.info(`  git commit -m "initial commit"`);
 
-  if (drizzleInUse) {
+  if (packages?.drizzle.inUse) {
     logger.warn(
       `\nThank you for trying out the new Drizzle option. If you encounter any issues, please open an issue!`,
       `\nNote: We use the PlanetScale driver so that you can query your data in edge runtimes. If you want to use a different driver, you'll need to change it yourself.`
