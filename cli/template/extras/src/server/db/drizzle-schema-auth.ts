07ab600b1a1b56363757fada784260489a2ef0e0
juliusmarminge <julius0216@outlook.com>
2023-08-13 22:40:24 +0200

    use `text`

--- cli/template/extras/src/server/db/drizzle-schema-auth.ts
+++ cli/template/extras/src/server/db/drizzle-schema-auth.ts
@@ -8,6 +8,7 @@ import {
   timestamp,
   uniqueIndex,
   varchar,
+  text,
 } from "drizzle-orm/mysql-core";
 import { type AdapterAccount } from "next-auth/adapters";
 
@@ -56,12 +57,12 @@ export const accounts = mysqlTable(
       .notNull(),
     provider: varchar("provider", { length: 255 }).notNull(),
     providerAccountId: varchar("providerAccountId", { length: 255 }).notNull(),
-    refresh_token: varchar("refresh_token", { length: 255 }),
-    access_token: varchar("access_token", { length: 255 }),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
     expires_at: int("expires_at"),
     token_type: varchar("token_type", { length: 255 }),
     scope: varchar("scope", { length: 255 }),
-    id_token: varchar("id_token", { length: 255 }),
+    id_token: text("id_token"),
     session_state: varchar("session_state", { length: 255 }),
   },
   (account) => ({
