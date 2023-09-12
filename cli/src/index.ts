b8507dc64ee91b8423de2ab6145cb3e7d8072528
Néstor <nestor@nstlopez.com>
2023-09-12 10:19:53 +0200

    feat: set packageManager on app creation (#1561)
    Co-authored-by: Julius Marminge <julius0216@outlook.com>

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import path from "path";
+import { execa } from "execa";
 import fs from "fs-extra";
 import { type PackageJson } from "type-fest";
 
@@ -9,6 +10,7 @@ import { initializeGit } from "~/helpers/git.js";
 import { logNextSteps } from "~/helpers/logNextSteps.js";
 import { setImportAlias } from "~/helpers/setImportAlias.js";
 import { buildPkgInstallerMap } from "~/installers/index.js";
+import { getUserPkgManager } from "~/utils/getUserPkgManager.js";
 import { logger } from "~/utils/logger.js";
 import { parseNameAndPath } from "~/utils/parseNameAndPath.js";
 import { renderTitle } from "~/utils/renderTitle.js";
@@ -27,6 +29,7 @@ type CT3APackageJSON = PackageJson & {
 
 const main = async () => {
   const npmVersion = await getNpmVersion();
+  const pkgManager = getUserPkgManager();
   renderTitle();
   npmVersion && renderVersionWarning(npmVersion);
 
@@ -55,6 +58,15 @@ const main = async () => {
   ) as CT3APackageJSON;
   pkgJson.name = scopedAppName;
   pkgJson.ct3aMetadata = { initVersion: getVersion() };
+
+  // ? Bun doesn't support this field (yet)
+  if (pkgManager !== "bun") {
+    const { stdout } = await execa(pkgManager, ["-v"], {
+      cwd: projectDir,
+    });
+    pkgJson.packageManager = `${pkgManager}@${stdout.trim()}`;
+  }
+
   fs.writeJSONSync(path.join(projectDir, "package.json"), pkgJson, {
     spaces: 2,
   });
