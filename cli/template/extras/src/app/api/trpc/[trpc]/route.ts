a8049c7ccc9335efddf87149338d99eead89f31b
juliusmarminge <julius0216@outlook.com>
2023-09-13 11:58:40 +0200

    fix type

--- cli/template/extras/src/app/api/trpc/[trpc]/route.ts
+++ cli/template/extras/src/app/api/trpc/[trpc]/route.ts
@@ -1,10 +1,11 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { type NextRequest } from "next/server";
 
 import { env } from "~/env.mjs";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 
-const handler = (req: Request) =>
+const handler = (req: NextRequest) =>
   fetchRequestHandler({
     endpoint: "/api/trpc",
     req,
