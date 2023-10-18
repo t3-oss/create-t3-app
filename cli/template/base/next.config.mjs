f0739a129ed7c74c9d148abc545390a1edceb897
c-ehrlich <ehrlich.christopher@gmail.com>
2023-10-18 22:17:35 +0200

    check node version in next config

--- cli/template/base/next.config.mjs
+++ cli/template/base/next.config.mjs
@@ -4,6 +4,8 @@
  */
 await import("./src/env.mjs");
 
+checkNodeVersion();
+
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
@@ -20,3 +22,14 @@ const config = {
 };
 
 export default config;
+
+function checkNodeVersion() {
+  const nodeVersion = process.versions.node;
+  const [major, minor] = nodeVersion.split(".").map((n) => parseInt(n));
+  if (!major || !minor) {
+    throw new Error(`Unable to parse Node version: ${nodeVersion}`);
+  }
+  if (major < 18 || (major === 18 && minor < 6) || (major === 19 && minor < 7)) {
+    throw new Error(`You are running an outdated Node version. Please use at least 18.16, 19.7, or 20.0`);
+  }
+}
