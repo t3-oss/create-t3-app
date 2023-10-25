716a1ad42b1f49119eeb0b8193406682ec105b8d
Jacksonmills <jacksonemills@gmail.com>
2023-10-25 18:01:42 -0500

    Add debug line to index.ts in cli/src

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -41,6 +41,8 @@ const main = async () => {
 
   const usePackages = buildPkgInstallerMap(packages);
 
+  // DEBUG LINE
+
   // e.g. dir/@mono/app returns ["@mono/app", "dir/app"]
   const [scopedAppName, appDir] = parseNameAndPath(appName);
 
