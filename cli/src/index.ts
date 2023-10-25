a89e8ec397ed1537c3110d1076fb86c9d5c3afc8
Jacksonmills <jacksonemills@gmail.com>
2023-10-25 17:54:52 -0500

    revert change

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -41,8 +41,6 @@ const main = async () => {
 
   const usePackages = buildPkgInstallerMap(packages);
 
-  // DEBUG
-
   // e.g. dir/@mono/app returns ["@mono/app", "dir/app"]
   const [scopedAppName, appDir] = parseNameAndPath(appName);
 
