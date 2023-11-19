36a2d5898d12fc13e299abafa9e8c3eeb2244f6a
juliusmarminge <julius0216@outlook.com>
2023-11-19 11:26:46 +0100

    revert drizzle bump

--- cli/src/installers/dependencyVersionMap.ts
+++ cli/src/installers/dependencyVersionMap.ts
@@ -13,7 +13,7 @@ export const dependencyVersionMap = {
   "@prisma/client": "^5.6.0",
 
   // Drizzle
-  "drizzle-orm": "^0.29.0",
+  "drizzle-orm": "^0.28.5",
   "drizzle-kit": "^0.19.3",
   "dotenv-cli": "^7.3.0",
   mysql2: "^3.6.1",
