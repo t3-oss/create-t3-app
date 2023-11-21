32e0747a027be16cc36ad5bead062eea696e4012
Julius Marminge <julius0216@outlook.com>
2023-11-21 23:48:15 +0100

    feat: skip http for trpc rsc calls (#1670)

--- cli/template/base/_eslintrc.cjs
+++ cli/template/base/_eslintrc.cjs
@@ -24,6 +24,7 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/require-await": "off",
     "@typescript-eslint/no-misused-promises": [
       "error",
       {
