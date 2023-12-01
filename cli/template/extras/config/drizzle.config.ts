a9d842157c2df3a1c2e581e9e57ca7d955a654d3
mikah13 <anhminhhoang13@gmail.com>
2023-12-01 10:56:06 -0800

    add: out dir for migrations

--- cli/template/extras/config/drizzle.config.ts
+++ cli/template/extras/config/drizzle.config.ts
@@ -4,6 +4,7 @@ import { env } from "~/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
+  out: "./drizzle/migrations",
   driver: "mysql2",
   dbCredentials: {
     connectionString: env.DATABASE_URL,
