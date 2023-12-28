5dc99df07cbdd042232bc9278a3a951db0dfacc1
juliusmarminge <julius0216@outlook.com>
2023-12-28 16:09:37 +0100

    rev some

--- cli/template/base/_eslintrc.cjs
+++ cli/template/base/_eslintrc.cjs
@@ -6,7 +6,7 @@ const config = {
   },
   plugins: ["@typescript-eslint"],
   extends: [
-    "next/core-web-vitals",
+    "@next/next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
   ],
