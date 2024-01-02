be53f2c5ad33a8023a5f2e7a3c6ea1c54b57eb62
JOYBOY-0 <falamino00@gmail.com>
2024-01-02 15:59:54 -0300

    fix postinstall

--- cli/src/installers/basehub.ts
+++ cli/src/installers/basehub.ts
@@ -13,11 +13,14 @@ export const basehubInstaller: Installer = ({ projectDir }) => {
   });
 
   // add postinstall script to package.json
-  const packageJsonPath = path.join(projectDir, "package.json");
+  const pkgJson = fs.readJSONSync(
+    path.join(projectDir, "package.json")
+  ) as PackageJson;
 
-  const packageJsonContent = fs.readJSONSync(packageJsonPath) as PackageJson;
-  packageJsonContent.scripts = {
-    ...packageJsonContent.scripts,
-    postinstall: "basehub",
-  };
+  pkgJson.scripts = pkgJson.scripts || {};
+
+  pkgJson.scripts.postinstall = "basehub";
+  fs.writeJSONSync(path.join(projectDir, "package.json"), pkgJson, {
+    spaces: 2,
+  });
 };
