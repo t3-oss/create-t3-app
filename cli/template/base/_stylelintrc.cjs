4ef16f3e55573b1ce943a335ffae1f3c5754f7d0
JOYBOY-0 <falamino00@gmail.com>
2024-01-03 15:06:47 -0300

    upd deps and rules

--- cli/template/base/_stylelintrc.cjs
+++ cli/template/base/_stylelintrc.cjs
@@ -1,12 +1,12 @@
 const config = {
   ignoreFiles: ["**/*.{ts,tsx,js,jsx}"],
   extends: [
+    "stylelint-prettier",
     "stylelint-config-standard",
     "stylelint-config-standard-scss",
-    "stylelint-prettier/recommended",
   ],
-  plugins: ["stylelint-prettier"],
   rules: {
+    "import-notation": ["string"],
     "scss/at-rule-no-unknown": [
       true,
       {
@@ -42,18 +42,6 @@ const config = {
         ignoreProperties: ["font-named-instance"],
       },
     ],
-    "declaration-block-trailing-semicolon": null,
-    "no-descending-specificity": null,
-    "number-leading-zero": null,
-    "length-zero-no-unit": null,
-    "alpha-value-notation": null,
-    "selector-id-pattern": null,
-    "selector-class-pattern": null,
-    "property-no-vendor-prefix": null,
-    "value-no-vendor-prefix": null,
-    "scss/no-global-function-names": null,
-    "scss/double-slash-comment-empty-line-before": null,
   },
 };
-
 module.exports = config;
