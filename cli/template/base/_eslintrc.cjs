cc3fa248f05719e7163b6b2c37003c8df08ceb91
JOYBOY-0 <falamino00@gmail.com>
2024-01-03 12:56:06 -0300

    upd eslint rules

--- cli/template/base/_eslintrc.cjs
+++ cli/template/base/_eslintrc.cjs
@@ -1,37 +1,88 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: true,
+    project: true
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'simple-import-sort'],
   extends: [
-    "plugin:@next/next/recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:@next/next/recommended'
   ],
-  rules: {
-    // These opinionated rules are enabled in stylistic-type-checked above.
-    // Feel free to reconfigure them to your own preference.
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/require-await": "off",
-    "@typescript-eslint/no-misused-promises": [
-      "error",
-      {
-        checksVoidReturn: { attributes: false },
-      },
-    ],
+  ignorePatterns: [],
+  env: {
+    es6: true,
+    browser: true,
+    node: true
   },
-};
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/display-name': 0,
+    'react/prop-types': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-member-accessibility': 0,
+    '@typescript-eslint/indent': 0,
+    '@typescript-eslint/member-delimiter-style': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-use-before-define': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
+    'react-hooks/exhaustive-deps': [
+      'warn',
+      {
+        additionalHooks: '(useIsomorphicLayoutEffect|useGsapContext)'
+      }
+    ],
+    'react/no-unescaped-entities': 0,
+    curly: ['error', 'multi-line'],
+    'react/jsx-no-target-blank': [
+      2,
+      {
+        allowReferrer: true
+      }
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      2,
+      {
+        argsIgnorePattern: '^_'
+      }
+    ],
+    'no-console': [
+      1,
+      {
+        allow: ['warn', 'error']
+      }
+    ],
+    'prettier/prettier': [
+      'warn',
+      {
+        endOfLine: 'auto'
+      }
+    ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: '.'
+      }
+    },
+    react: {
+      version: 'detect'
+    }
+  }
+}
 
-module.exports = config;
+module.exports = config
