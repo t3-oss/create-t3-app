ed731955a27d2dd301c895552338d31896261889
juliusmarminge <julius0216@outlook.com>
2024-01-04 16:56:14 +0100

    rm cast for prisma adapter

--- cli/template/extras/src/server/auth-app/with-prisma.ts
+++ cli/template/extras/src/server/auth-app/with-prisma.ts
@@ -4,7 +4,6 @@ import {
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
-import { type Adapter } from "next-auth/adapters";
 import DiscordProvider from "next-auth/providers/discord";
 
 import { env } from "~/env";
@@ -46,7 +45,7 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   },
-  adapter: PrismaAdapter(db) as Adapter,
+  adapter: PrismaAdapter(db),
   providers: [
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
