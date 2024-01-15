c116281f3626ae5f05191037789d7338a8fa2e5c
Julius Marminge <julius0216@outlook.com>
2024-01-15 21:24:09 +0100

    fix: no-cookies-for-cc (#1723)
    Co-authored-by: Christopher Ehrlich <ehrlich.christopher@gmail.com>

--- cli/template/extras/src/app/page/with-auth-trpc-tw.tsx
+++ cli/template/extras/src/app/page/with-auth-trpc-tw.tsx
@@ -1,3 +1,4 @@
+import { unstable_noStore as noStore } from "next/cache";
 import Link from "next/link";
 
 import { CreatePost } from "~/app/_components/create-post";
@@ -5,6 +6,7 @@ import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
 export default async function Home() {
+  noStore();
   const hello = await api.post.hello.query({ text: "from tRPC" });
   const session = await getServerAuthSession();
 
