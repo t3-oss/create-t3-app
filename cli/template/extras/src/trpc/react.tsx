3af78ac4e5cb392cde4b02f9946fc6dcce93717e
Julius Marminge <julius0216@outlook.com>
2024-02-15 01:56:25 +0100

    move-transformer-1

--- cli/template/extras/src/trpc/react.tsx
+++ cli/template/extras/src/trpc/react.tsx
@@ -15,7 +15,6 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
 
   const [trpcClient] = useState(() =>
     api.createClient({
-      transformer: SuperJSON,
       links: [
         loggerLink({
           enabled: (op) =>
@@ -23,6 +22,7 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
             (op.direction === "down" && op.result instanceof Error),
         }),
         unstable_httpBatchStreamLink({
+          transformer: SuperJSON,
           url: getBaseUrl() + "/api/trpc",
           async headers() {
             const headers = new Headers();
