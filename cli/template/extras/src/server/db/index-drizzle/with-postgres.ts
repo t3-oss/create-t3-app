ff3c86126d16011d9153c62aa16a4681db8aa26d
Alexis Tacnet <alexistacnet@gmail.com>
2024-02-20 09:36:54 +0100

    fix: fix too many clients error on Postgres due to HMR with Drizzle

--- cli/template/extras/src/server/db/index-drizzle/with-postgres.ts
+++ cli/template/extras/src/server/db/index-drizzle/with-postgres.ts
@@ -1,7 +1,23 @@
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle, type PostgresJsDatabase } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 import { env } from "~/env.js";
 import * as schema from "./schema";
 
-export const db = drizzle(postgres(env.DATABASE_URL), { schema });
+const global: {
+  db: PostgresJsDatabase<typeof schema> | undefined;
+} = {
+  db: undefined,
+};
+
+let db: PostgresJsDatabase<typeof schema>;
+
+if (env.NODE_ENV === "production") {
+  db = drizzle(postgres(env.DATABASE_URL), { schema });
+} else {
+  if (!global.db) global.db = drizzle(postgres(env.DATABASE_URL), { schema });
+
+  db = global.db;
+}
+
+export { db };
