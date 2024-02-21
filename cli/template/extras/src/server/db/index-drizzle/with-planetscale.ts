643c0b53a03222fa3949a4c0b305f61793615f97
Alexis Tacnet <alexistacnet@gmail.com>
2024-02-21 09:43:34 +0100

    Put back Planetscale connection management with Drizzle

--- cli/template/extras/src/server/db/index-drizzle/with-planetscale.ts
+++ cli/template/extras/src/server/db/index-drizzle/with-planetscale.ts
@@ -1,23 +1,12 @@
 import { Client } from "@planetscale/database";
-import {
-  drizzle,
-  type PlanetScaleDatabase,
-} from "drizzle-orm/planetscale-serverless";
+import { drizzle } from "drizzle-orm/planetscale-serverless";
 
 import { env } from "~/env";
 import * as schema from "./schema";
 
-const globalForDrizzle = globalThis as unknown as {
-  db: PlanetScaleDatabase<typeof schema> | undefined;
-};
-
-export const db =
-  globalForDrizzle.db ??
-  drizzle(
-    new Client({
-      url: env.DATABASE_URL,
-    }).connection(),
-    { schema }
-  );
-
-if (env.NODE_ENV !== "production") globalForDrizzle.db = db;
+export const db = drizzle(
+  new Client({
+    url: env.DATABASE_URL,
+  }).connection(),
+  { schema }
+);
