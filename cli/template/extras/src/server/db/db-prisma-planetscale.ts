586b1dc34f41d9308a1cdcbe9f79d1fd0dc63e94
Julius Marminge <julius0216@outlook.com>
2024-02-22 18:28:18 +0100

    feat: support prisma extends (#1773)

--- cli/template/extras/src/server/db/db-prisma-planetscale.ts
+++ cli/template/extras/src/server/db/db-prisma-planetscale.ts
@@ -2,20 +2,21 @@ import { Client } from "@planetscale/database";
 import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 import { PrismaClient } from "@prisma/client";
 
-import { env } from "~/env.js";
+import { env } from "~/env";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const psClient = new Client({ url: env.DATABASE_URL });
 
-const client = new Client({ url: env.DATABASE_URL });
-
-export const db =
-  globalForPrisma.prisma ??
+const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    adapter: new PrismaPlanetScale(client),
+    adapter: new PrismaPlanetScale(psClient),
   });
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient> | undefined;
+};
+
+export const db = globalForPrisma.prisma ?? createPrismaClient();
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
