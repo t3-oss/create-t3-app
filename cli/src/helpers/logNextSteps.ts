d15ba430477186a0b939823a2b76253238f39c40
Liam Doyle <liamdoyle@limegorilla.co.uk>
2024-02-22 22:17:57 +0000

    Only show error if not using `mysql`

--- cli/src/helpers/logNextSteps.ts
+++ cli/src/helpers/logNextSteps.ts
@@ -63,7 +63,7 @@ export const logNextSteps = async ({
     );
   }
 
-  if (packages?.drizzle.inUse) {
+  if (packages?.drizzle.inUse && !["mysql"].includes(databaseProvider) {
     logger.warn(
       `\nThank you for trying out the new Drizzle option. If you encounter any issues, please open an issue!`,
       `\nNote: We use the PlanetScale driver so that you can query your data in edge runtimes. If you want to use a different driver, you'll need to change it yourself.`
