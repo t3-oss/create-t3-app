0418663ec6cc370ed1e067f1ef68f0d5ca41af53
Julius Marminge <julius0216@outlook.com>
2024-02-22 18:04:35 +0100

    chore: update drizzle (#1774)

--- cli/template/extras/src/server/db/index-drizzle/with-planetscale.ts
+++ cli/template/extras/src/server/db/index-drizzle/with-planetscale.ts
@@ -4,9 +4,4 @@ import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { env } from "~/env";
 import * as schema from "./schema";
 
-export const db = drizzle(
-  new Client({
-    url: env.DATABASE_URL,
-  }).connection(),
-  { schema }
-);
+export const db = drizzle(new Client({ url: env.DATABASE_URL }), { schema });
