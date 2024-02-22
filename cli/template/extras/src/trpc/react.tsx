3b974216044bbf37d633bd2d53dcb355ccf40390
juliusmarminge <julius0216@outlook.com>
2024-02-22 17:30:32 +0100

    rm unnecessary else

--- cli/template/extras/src/trpc/react.tsx
+++ cli/template/extras/src/trpc/react.tsx
@@ -15,10 +15,9 @@ const getQueryClient = () => {
   if (typeof window === "undefined") {
     // Server: always make a new query client
     return createQueryClient();
-  } else {
-    // Browser: use singleton pattern to keep the same query client
-    return (clientQueryClientSingleton ??= createQueryClient());
   }
+  // Browser: use singleton pattern to keep the same query client
+  return (clientQueryClientSingleton ??= createQueryClient());
 };
 
 export const api = createTRPCReact<AppRouter>();
