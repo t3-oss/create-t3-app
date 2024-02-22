c1037228e658b48a9f93c2f0d058a8ce25288078
juliusmarminge <julius0216@outlook.com>
2024-02-22 21:30:28 +0100

    more bumps

--- cli/src/installers/dependencyVersionMap.ts
+++ cli/src/installers/dependencyVersionMap.ts
@@ -5,13 +5,13 @@
 export const dependencyVersionMap = {
   // NextAuth.js
   "next-auth": "^4.24.5",
-  "@next-auth/prisma-adapter": "^1.0.7",
-  "@auth/drizzle-adapter": "^0.3.6",
+  "@auth/prisma-adapter": "^1.4.0",
+  "@auth/drizzle-adapter": "^0.7.0",
 
   // Prisma
-  prisma: "^5.6.0",
-  "@prisma/client": "^5.6.0",
-  "@prisma/adapter-planetscale": "^5.6.0",
+  prisma: "^5.10.2",
+  "@prisma/client": "^5.10.2",
+  "@prisma/adapter-planetscale": "^5.10.2",
 
   // Drizzle
   "drizzle-orm": "^0.29.4",
@@ -24,17 +24,16 @@ export const dependencyVersionMap = {
   "better-sqlite3": "^9.0.0",
 
   // TailwindCSS
-  tailwindcss: "^3.3.5",
-  autoprefixer: "^10.4.14",
-  postcss: "^8.4.31",
-  prettier: "^3.1.0",
-  "prettier-plugin-tailwindcss": "^0.5.7",
+  tailwindcss: "^3.4.1",
+  postcss: "^8.4.34",
+  prettier: "^3.2.5",
+  "prettier-plugin-tailwindcss": "^0.5.11",
 
   // tRPC
-  "@trpc/client": "^10.43.6",
-  "@trpc/server": "^10.43.6",
-  "@trpc/react-query": "^10.43.6",
-  "@trpc/next": "^10.43.6",
+  "@trpc/client": "^10.45.1",
+  "@trpc/server": "^10.45.1",
+  "@trpc/react-query": "^10.45.1",
+  "@trpc/next": "^10.45.1",
   "@tanstack/react-query": "^4.36.1",
   superjson: "^2.2.1",
   "server-only": "^0.0.1",
