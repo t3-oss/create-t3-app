d52b86ebf6ff199e8f62d44ae5347f6fc7775dd4
Timon Jurschitsch <timon.jurschitsch@gmail.com>
2024-02-23 18:33:23 +0100

    fix: missing default font in pagesDir

--- cli/template/extras/src/pages/_app/with-trpc.tsx
+++ cli/template/extras/src/pages/_app/with-trpc.tsx
@@ -1,11 +1,20 @@
 import { type AppType } from "next/app";
+import { Inter } from "next/font/google";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
+const inter = Inter({
+  subsets: ["latin"],
+});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default api.withTRPC(MyApp);
