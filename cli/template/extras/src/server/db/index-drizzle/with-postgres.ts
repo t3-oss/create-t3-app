aabce8a66516212bc797f77757f21acbf503a1ee
juliusmarminge <julius0216@outlook.com>
2024-03-01 17:26:59 +0100

    consistent

--- cli/template/extras/src/server/db/index-drizzle/with-postgres.ts
+++ cli/template/extras/src/server/db/index-drizzle/with-postgres.ts
@@ -8,11 +8,11 @@ import * as schema from "./schema";
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
-const globalForDrizzle = globalThis as unknown as {
+const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
 
-const conn = globalForDrizzle.conn ?? postgres(env.DATABASE_URL);
-if (env.NODE_ENV !== "production") globalForDrizzle.conn = conn;
+const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
+if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const db = drizzle(conn, { schema });
--- cli/template/extras/src/server/db/index-drizzle/with-sqlite.ts
+++ cli/template/extras/src/server/db/index-drizzle/with-sqlite.ts
@@ -8,14 +8,12 @@ import * as schema from "./schema";
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
-const globalForDrizzle = globalThis as unknown as {
+const globalForDb = globalThis as unknown as {
   conn: Database | undefined;
 };
 
 export const conn =
-  globalForDrizzle.conn ??
-  new Database(env.DATABASE_URL, { fileMustExist: false });
-
-if (env.NODE_ENV !== "production") globalForDrizzle.conn = conn;
+  globalForDb.conn ?? new Database(env.DATABASE_URL, { fileMustExist: false });
+if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const db = drizzle(conn, { schema });
