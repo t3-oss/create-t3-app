9806b822462d91a550fc9295d3b88ca4aa064ca3
thecmdrunner <38887390+thecmdrunner@users.noreply.github.com>
2024-03-02 23:07:37 +0530

    feat: add eslint installer

---
+++ cli/src/installers/eslint.ts
@@ -0,0 +1,38 @@
+import path from "path";
+import fs from "fs-extra";
+
+import { type Installer } from "~/installers/index.js";
+
+export const dynamicEslintInstaller: Installer = ({ projectDir, packages }) => {
+  const usingDrizzle = !!packages?.drizzle?.inUse;
+
+  const _eslintConfigSrc = path.join(projectDir, "_eslint.config.json");
+
+  const eslintConfig = JSON.parse(
+    fs.readFileSync(_eslintConfigSrc).toString()
+  ) as Record<string, unknown>;
+
+  if (usingDrizzle) {
+    eslintConfig.plugins = [...(eslintConfig.plugins as string[]), "drizzle"];
+
+    const lintRules = {
+      "drizzle/enforce-delete-with-where": "error",
+      "drizzle/enforce-update-with-where": "error",
+    };
+
+    eslintConfig.rules = {
+      ...(eslintConfig.rules as Record<string, unknown>),
+      ...lintRules,
+    };
+  }
+
+  // Convert config from _eslint.config.json to .eslintrc.cjs
+  const eslintrcFileContents = [
+    '/** @type {import("eslint").Linter.Config} */',
+    "const config = " + JSON.stringify(eslintConfig, null, 2),
+    "module.exports = config;",
+  ].join("\n");
+
+  const eslintConfigDest = path.join(projectDir, ".eslintrc.cjs");
+  fs.writeFileSync(eslintConfigDest, eslintrcFileContents, "utf-8");
+};
