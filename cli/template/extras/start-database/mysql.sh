8ff2e5bc3b31cbebc5d632eea2b00983cdfa9a37
Kaspar Emanuel <kaspar@kitspace.org>
2024-04-08 00:56:49 +0100

    fix: improve start-databse scripts exit and messages (#1816)

--- cli/template/extras/start-database/mysql.sh
+++ cli/template/extras/start-database/mysql.sh
@@ -12,13 +12,18 @@
 DB_CONTAINER_NAME="project1-mysql"
 
 if ! [ -x "$(command -v docker)" ]; then
-  echo "Docker is not installed. Please install docker and try again.\nDocker install guide: https://docs.docker.com/engine/install/"
+  echo -e "Docker is not installed. Please install docker and try again.\nDocker install guide: https://docs.docker.com/engine/install/"
   exit 1
 fi
 
 if [ "$(docker ps -q -f name=$DB_CONTAINER_NAME)" ]; then
-  docker start $DB_CONTAINER_NAME
-  echo "Database container started"
+  echo "Database container '$DB_CONTAINER_NAME' already running"
+  exit 0
+fi
+
+if [ "$(docker ps -q -a -f name=$DB_CONTAINER_NAME)" ]; then
+  docker start "$DB_CONTAINER_NAME"
+  echo "Existing database container '$DB_CONTAINER_NAME' started"
   exit 0
 fi
 
@@ -26,7 +31,7 @@ fi
 set -a
 source .env
 
-DB_PASSWORD=$(echo $DATABASE_URL | awk -F':' '{print $3}' | awk -F'@' '{print $1}')
+DB_PASSWORD=$(echo "$DATABASE_URL" | awk -F':' '{print $3}' | awk -F'@' '{print $1}')
 
 if [ "$DB_PASSWORD" == "password" ]; then
   echo "You are using the default database password"
@@ -40,6 +45,9 @@ if [ "$DB_PASSWORD" == "password" ]; then
   sed -i -e "s#:password@#:$DB_PASSWORD@#" .env
 fi
 
-docker run --name $DB_CONTAINER_NAME -e MYSQL_ROOT_PASSWORD=$DB_PASSWORD -e MYSQL_DATABASE=project1 -d -p 3306:3306 docker.io/mysql
-
-echo "Database container was successfully created"
+docker run -d \
+  --name $DB_CONTAINER_NAME \
+  -e MYSQL_ROOT_PASSWORD="$DB_PASSWORD" \
+  -e MYSQL_DATABASE=project1 \
+  -p 3306:3306 \
+  docker.io/mysql && echo "Database container '$DB_CONTAINER_NAME' was successfully created"
