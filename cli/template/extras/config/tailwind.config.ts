0a56de2545a6fd905c46410e4df5738080dd3c31
kakeWasTaken <ethanzhou100@gmail.com>
2024-05-02 14:11:47 -0700

    fix: add requested fixes and changes

--- cli/template/extras/config/tailwind.config.ts
+++ cli/template/extras/config/tailwind.config.ts
@@ -1,11 +1,12 @@
 import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-geist-sans)"],
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
     },
   },
