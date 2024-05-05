afcf035c25af787734ca1ed587dd85b6e97eb624
Pranav <hey@pranava.dev>
2024-05-05 23:55:13 +0530

    fix: dir not found

--- cli/src/installers/vscode.ts
+++ cli/src/installers/vscode.ts
@@ -1,13 +1,7 @@
-import path from "path";
 import fs from "fs-extra";
 
 import { type Installer } from "~/installers/index.js";
 
-const _vscodeSettingsPath = path.resolve(
-  __dirname,
-  "../../template/extras/config/.vscode"
-);
-
 export const vscodeInstaller: Installer = ({ projectDir }) => {
-  fs.moveSync(_vscodeSettingsPath, path.join(projectDir, ".vscode"));
+  fs.copySync("cli/template/extras/config/.vscode", projectDir + "/.vscode");
 };
