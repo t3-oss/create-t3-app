7f9dc48100935d4a11fd1d0f7ee9dc1fa58278e4
Asher Myers <43706372+ashermyers@users.noreply.github.com>
2024-07-01 23:54:42 -0400

    Update base.ts

--- cli/template/extras/src/server/api/routers/post/base.ts
+++ cli/template/extras/src/server/api/routers/post/base.ts
@@ -19,9 +19,6 @@ export const postRouter = createTRPCRouter({
   create: publicProcedure
     .input(z.object({ name: z.string().min(1) }))
     .mutation(async ({ input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
       post = { id: post.id + 1, name: input.name };
       return post;
     }),
