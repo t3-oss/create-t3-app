4b0f33c431f3779abeefdf9389a460318455adfc
Asher Myers <43706372+ashermyers@users.noreply.github.com>
2024-07-01 23:55:17 -0400

    Update with-drizzle.ts

--- cli/template/extras/src/server/api/routers/post/with-drizzle.ts
+++ cli/template/extras/src/server/api/routers/post/with-drizzle.ts
@@ -15,9 +15,6 @@ export const postRouter = createTRPCRouter({
   create: publicProcedure
     .input(z.object({ name: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
       await ctx.db.insert(posts).values({
         name: input.name,
       });
