340cfc0255034e05c71d358bbf151e1d096e8406
juliusmarminge <julius0216@outlook.com>
2024-07-05 00:18:52 +0200

    trpc prefetching

--- cli/template/extras/src/trpc/server.ts
+++ cli/template/extras/src/trpc/server.ts
@@ -1,10 +1,12 @@
 import "server-only";
 
+import { createHydrationHelpers } from "@trpc/react-query/rsc";
 import { headers } from "next/headers";
 import { cache } from "react";
 
-import { createCaller } from "~/server/api/root";
+import { createCaller, type AppRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
+import { createQueryClient } from "./query-client";
 
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
@@ -19,4 +21,10 @@ const createContext = cache(() => {
   });
 });
 
-export const api = createCaller(createContext);
+const getQueryClient = cache(createQueryClient);
+const caller = createCaller(createContext);
+
+export const { trpc: api, HydrateClient } = createHydrationHelpers<AppRouter>(
+  caller,
+  getQueryClient
+);
