5a0cbc53afb8597d6da2bc6d184ea1427e741cec
Julius Marminge <julius0216@outlook.com>
2024-07-07 11:17:00 +0200

    chore: bump some deps (#1937)

--- cli/template/extras/src/app/page/with-trpc-tw.tsx
+++ cli/template/extras/src/app/page/with-trpc-tw.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
