c3dba7e6576bb01c8873787ac1b3e327615515f0
Aiden Cline <aidenpcline@gmail.com>
2024-09-20 15:56:45 -0500

    update some docs stuff

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,7 +1,7 @@
+import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeExternalLinks from "rehype-external-links";
@@ -12,8 +12,9 @@ import remarkCodeTitles from "remark-code-titles";
 export default defineConfig({
   site: `https://create.t3.gg/`,
   output: "server",
-  adapter: vercel(),
+  adapter: cloudflare(),
   markdown: {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     remarkPlugins: [remarkCodeTitles],
     rehypePlugins: [
       [
