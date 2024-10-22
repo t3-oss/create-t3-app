b5ca0c63632cb3699b26f077a1952caf3ac267c3
Matvey Ryabchikov <35634442+ronanru@users.noreply.github.com>
2024-10-22 15:02:54 +0300

    feat: update to next 15

--- cli/template/base/next.config.ts
+++ cli/template/base/next.config.ts
@@ -1,23 +1,18 @@
+import { type NextConfig } from "next";
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+import "./src/env";
 
-/** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
   transpilePackages: ["geist"],
-};
+} satisfies NextConfig;
 
 export default config;
--- cli/template/extras/config/next-config-appdir.ts
+++ cli/template/extras/config/next-config-appdir.ts
@@ -1,10 +1,11 @@
+import { type NextConfig } from "next";
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+import "./src/env";
 
-/** @type {import("next").NextConfig} */
-const config = {};
+const config = {} satisfies NextConfig;
 
 export default config;
