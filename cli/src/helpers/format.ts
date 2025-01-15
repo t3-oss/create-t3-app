08173ced6eb8b8a1fa51e791be4cec17bcc9093b
aidansunbury <aidansunbury@gmail.com>
2025-01-15 15:34:19 -0800

    lint fix

--- cli/src/helpers/format.ts
+++ cli/src/helpers/format.ts
@@ -2,7 +2,7 @@ import chalk from "chalk";
 import { execa } from "execa";
 import ora from "ora";
 
-import type { PackageManager } from "~/utils/getUserPkgManager.js";
+import { type PackageManager } from "~/utils/getUserPkgManager.js";
 import { logger } from "~/utils/logger.js";
 
 // Runs format and lint command to ensure created repository is tidy upon creation
