b27e276017992051b96112645305d599319be2ad
ahkhanjani <72540492+ahkhanjani@users.noreply.github.com>
2025-02-22 20:07:26 +0330

    "upgrade" app to next15

--- upgrade/next.config.js
+++ upgrade/next.config.js
@@ -10,7 +10,6 @@ import { getHighlighter } from "shiki";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
 };
