efe4c63f34959d32fdb5754274170c14fed2e402
juliusmarminge <jmarminge@gmail.com>
2025-03-22 00:41:13 +0100

    r19 init

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,7 +1,7 @@
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeExternalLinks from "rehype-external-links";
