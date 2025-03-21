519fac5a32b29704dc94ec1bd4bf938f722cd5c1
Julius Marminge <julius0216@outlook.com>
2025-03-21 23:03:06 +0100

    chore: nuke upgrade, use next/font (#2074)

--- upgrade/src/components/mdx.tsx
+++
@@ -1,8 +0,0 @@
-export function Steps(props: React.ComponentProps<"div">) {
-  return (
-    <div
-      className="[&>h3]:step ml-4 border-l pl-6 [counter-reset:step]"
-      {...props}
-    />
-  );
-}
