59356c7f76b6b15e0f062287714675a9e8cda282
hengrv <hengro44@gmail.com>
2025-03-26 19:49:46 +0000

    fix: add nullish coalescing to fix eslint error

--- cli/template/extras/src/trpc/react.tsx
+++ cli/template/extras/src/trpc/react.tsx
@@ -17,9 +17,8 @@ const getQueryClient = () => {
     return createQueryClient();
   }
   // Browser: use singleton pattern to keep the same query client
-  if (!clientQueryClientSingleton) {
-    clientQueryClientSingleton = createQueryClient();
-  }
+  clientQueryClientSingleton ??= createQueryClient();
+
   return clientQueryClientSingleton;
 };
 
