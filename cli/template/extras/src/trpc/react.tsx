51a1e30fb423bbe8c81a258627b7132be32d17a7
Matheus Castro <matheuscastro@gmail.com>
2025-03-26 10:08:14 -0300

    fix: lint error about nullish coalescing operator

--- cli/template/extras/src/trpc/react.tsx
+++ cli/template/extras/src/trpc/react.tsx
@@ -17,9 +17,7 @@ const getQueryClient = () => {
     return createQueryClient();
   }
   // Browser: use singleton pattern to keep the same query client
-  if (!clientQueryClientSingleton) {
-    clientQueryClientSingleton = createQueryClient();
-  }
+  clientQueryClientSingleton ??= createQueryClient();
   return clientQueryClientSingleton;
 };
 
