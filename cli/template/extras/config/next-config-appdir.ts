2baadb71b2fb9d6159351b2c9e7b8d9f9ea7d704
Matheus Castro <matheuscastro@gmail.com>
2025-03-26 09:45:20 -0300

    fix: lint error about top-level type imports

--- cli/template/extras/config/next-config-appdir.ts
+++ cli/template/extras/config/next-config-appdir.ts
@@ -2,7 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import type { NextConfig } from "next";
+import { type NextConfig } from "next";
 import "./src/env";
 
 const nextConfig: NextConfig = {
