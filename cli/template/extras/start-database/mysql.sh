e72ee5049c421d2f04af280d35a4c96c3be4111d
Cal Courtney <calthejuggler@gmail.com>
2025-04-05 18:01:48 +0100

    fix: update mysql bash script to use correct db name

--- cli/template/extras/start-database/mysql.sh
+++ cli/template/extras/start-database/mysql.sh
@@ -17,7 +17,7 @@ source .env
 DB_PASSWORD=$(echo "$DATABASE_URL" | awk -F':' '{print $3}' | awk -F'@' '{print $1}')
 DB_PORT=$(echo "$DATABASE_URL" | awk -F':' '{print $4}' | awk -F'\/' '{print $1}')
 DB_NAME=$(echo "$DATABASE_URL" | awk -F'/' '{print $4}')
-DB_CONTAINER_NAME="$DB_NAME-postgres"
+DB_CONTAINER_NAME="$DB_NAME-mysql"
 
 if ! [ -x "$(command -v docker)" ] && ! [ -x "$(command -v podman)" ]; then
   echo -e "Docker or Podman is not installed. Please install docker or podman and try again.\nDocker install guide: https://docs.docker.com/engine/install/\nPodman install guide: https://podman.io/getting-started/installation"
