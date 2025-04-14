aebe036ba8e11e117431c1bd19382dec834dfd9b
Amir H. Khanjani <72540492+ahkhanjani@users.noreply.github.com>
2025-04-14 19:14:16 +0330

    chore: upgrade prisma (#2103)

---
+++ .changeset/seven-clocks-double.md
@@ -0,0 +1,5 @@
+---
+"create-t3-app": minor
+---
+
+Upgrade to Prisma 6.6.0
--- cli/template/extras/src/server/db/db-prisma-planetscale.ts
+++ cli/template/extras/src/server/db/db-prisma-planetscale.ts
@@ -1,16 +1,13 @@
-import { Client } from "@planetscale/database";
 import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 import { PrismaClient } from "@prisma/client";
 
 import { env } from "~/env";
 
-const psClient = new Client({ url: env.DATABASE_URL });
-
 const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    adapter: new PrismaPlanetScale(psClient),
+    adapter: new PrismaPlanetScale({ url: env.DATABASE_URL }),
   });
 
 const globalForPrisma = globalThis as unknown as {
