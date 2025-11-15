87bea920349db9b61f15598927d2e5209aa465e5
HimanshuKumarDutt094 <adasimobenio@gmail.com>
2025-11-15 17:29:49 +0530

    bump: astro ,run format and lint fix

--- cli/template/extras/src/app/page/with-better-auth-trpc-tw.tsx
+++ cli/template/extras/src/app/page/with-better-auth-trpc-tw.tsx
@@ -58,7 +58,7 @@ export default async function Home() {
               {!session ? (
                 <form>
                   <button
-                  type="submit"
+                    type="submit"
                     className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
                     formAction={async () => {
                       "use server";
@@ -80,7 +80,7 @@ export default async function Home() {
               ) : (
                 <form>
                   <button
-                  type="submit"
+                    type="submit"
                     className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
                     formAction={async () => {
                       "use server";
