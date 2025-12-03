969ebe92b561502df97bdedc4fd30fbbb1a54d1d
Himanshu Kumar Dutt <adasimobenio@gmail.com>
2025-12-03 22:14:01 +0530

    fix: bug fixes to better auth ,cli and prisma generated (#2167)

--- cli/template/extras/src/server/better-auth/config/base.ts
+++ cli/template/extras/src/server/better-auth/config/base.ts
@@ -1,9 +1,12 @@
 import { betterAuth } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  // Make sure nextCookies() is the last plugin in the array
+  plugins: [nextCookies()],
 });
 
 export type Session = typeof auth.$Infer.Session;
